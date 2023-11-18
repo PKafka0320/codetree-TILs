f5c408f5c46415317ca1e545c48be32f4bf15c93
PKafka <66552990+PKafka0320@users.noreply.github.com>
2023-11-18 21:05:02 +0900

    Title: 사각형 채우기 3, Time: 127ms, Memory: 10MB, Status: Passed - CodeTree

--- 231118/사각형 채우기 3/rectangle-fill-3.java
+++ 231118/사각형 채우기 3/rectangle-fill-3.java
@@ -5,12 +5,12 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        int answer = tabul(n);
+        long answer = tabul(n);
 
         System.out.println(answer);
     }
 
-    public static int tabul(int n) {
+    public static long tabul(int n) {
         if (n <= 1) {
             return 2;
         }
@@ -18,12 +18,16 @@ public class Main {
             return 7;
         }
 
-        int[] memo = new int[n + 1];
+        long[] memo = new long[n + 1];
+        memo[0] = 1;
         memo[1] = 2;
         memo[2] = 7;
 
         for (int idx = 3; idx <= n; idx++) {
-            memo[idx] = (memo[idx - 1] * 2 + memo[idx - 2] * 4) % 1_000_000_007;
+            memo[idx] = (memo[idx - 1] * 2 + memo[idx - 2] * 3) % 1_000_000_007;
+            for (int subIdx = idx - 3; subIdx >= 0; subIdx--) {
+                memo[idx] = (memo[idx] + memo[subIdx] * 2) % 1_000_000_007;
+            }
         }
         return memo[n];
     }
