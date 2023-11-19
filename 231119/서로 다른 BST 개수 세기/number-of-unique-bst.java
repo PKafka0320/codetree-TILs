c845fb01af368d3e55ee9f4baeddf9c80782f9bd
PKafka <66552990+PKafka0320@users.noreply.github.com>
2023-11-19 12:59:44 +0900

    Title: 서로 다른 BST 개수 세기, Time: 122ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 231119/서로 다른 BST 개수 세기/number-of-unique-bst.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int answer = bst(n);
+
+        System.out.println(answer);
+    }
+
+    public static int bst(int n) {
+        if (n <= 2) {
+            return n;
+        }
+        int[] memo = new int[n + 1];
+
+        memo[0] = 1;
+        memo[1] = 1;
+        memo[2] = 2;
+
+        for (int i = 3; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                memo[i] += memo[j] * memo[i - j];
+            }
+        }
+
+        return memo[n];
+    }
+}
