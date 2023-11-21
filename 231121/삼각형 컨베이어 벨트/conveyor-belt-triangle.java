9d03fc9b63e18803e8295e2612d6a86087be7911
PKafka <66552990+PKafka0320@users.noreply.github.com>
2023-11-21 11:13:18 +0900

    Title: 삼각형 컨베이어 벨트, Time: 229ms, Memory: 13MB, Status: Passed - CodeTree

--- 231121/삼각형 컨베이어 벨트/conveyor-belt-triangle.java
+++ 231121/삼각형 컨베이어 벨트/conveyor-belt-triangle.java
@@ -5,19 +5,19 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int t = sc.nextInt();
-        int[][] conv = new int[n][n];
+        int[][] conv = new int[3][n];
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < 3; i++) {
             for (int j = 0; j < n; j++) {
                 conv[i][j] = sc.nextInt();
             }
         }
 
         for (int i = 0; i < t; i++) {
-            move(conv);
+            move(conv, n);
         }
         
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < 3; i++) {
             for (int j = 0; j < n; j++) {
                 System.out.print(conv[i][j] + " ");
             }
@@ -25,11 +25,11 @@ public class Main {
         }
     }
 
-    public static void move(int[][] conv) {
+    public static void move(int[][] conv, int n) {
         int temp;
-        int stash = conv[conv.length - 1][conv.length - 1];
-        for (int i = 0; i < conv.length; i++) {
-            for (int j = 0; j < conv.length; j++) {
+        int stash = conv[2][n - 1];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < n; j++) {
                 temp = conv[i][j];
                 conv[i][j] = stash;
                 stash = temp;
