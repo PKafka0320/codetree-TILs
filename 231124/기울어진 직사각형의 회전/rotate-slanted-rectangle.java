e2a5b552108e6372fb52edc0f652934c69884dca
PKafka <66552990+PKafka0320@users.noreply.github.com>
2023-11-24 11:48:11 +0900

    Title: 기울어진 직사각형의 회전, Time: 340ms, Memory: 22MB, Status: Passed - CodeTree

--- 231124/기울어진 직사각형의 회전/rotate-slanted-rectangle.java
+++ 231124/기울어진 직사각형의 회전/rotate-slanted-rectangle.java
@@ -29,57 +29,31 @@ public class Main {
         }
     }
 
-    public static void rotate(int[][] grid, int r, int c, int m1, int m2, int m3, int m4, int dir) {
-        int[] dx = {-1, -1, 1, 1};
-        int[] dy = {1, -1, -1, 1};
+    public static void rotate(int[][] grid, int r, int c, int m1, int m2, int m3, int m4, int moveDir) {
+        int[] dx;
+        int[] dy;
+        int[] moveNums;
+        if(moveDir == 1) {
+            dx = new int[]{-1, -1, 1, 1};
+            dy = new int[]{1, -1, -1, 1};
+            moveNums = new int[]{m1, m2, m3, m4}; 
+        }
+        else {
+            dx = new int[]{-1, -1, 1, 1};
+            dy = new int[]{-1, 1, 1, -1};
+            moveNums = new int[]{m4, m3, m2, m1};
+        }
+
         int temp = grid[r][c];
-
-        if (dir == 0) {
-            for (int i = 0; i < m4; i++) {
-                grid[r][c] = grid[r - dx[3]][c - dy[3]];
-                r -= dx[3];
-                c -= dy[3];
-            }
-            for (int i = 0; i < m3; i++) {
-                grid[r][c] = grid[r - dx[2]][c - dy[2]];
-                r -= dx[2];
-                c -= dy[2];
-            }
-            for (int i = 0; i < m2; i++) {
-                grid[r][c] = grid[r - dx[1]][c - dy[1]];
-                r -= dx[1];
-                c -= dy[1];
-            }
-            for (int i = 0; i < m1 - 1; i++) {
-                grid[r][c] = grid[r - dx[0]][c - dy[0]];
-                r -= dx[0];
-                c -= dy[0];
+        for (int dir = 0; dir < 4; dir++) {
+            for (int count = 0; count < moveNums[dir]; count++) {
+                int nr = r + dx[dir];
+                int nc = c + dy[dir];
+                grid[r][c] = grid[nr][nc];
+                r = nr;
+                c = nc;
             }
         }
-
-        if (dir == 1) {
-            for (int i = 0; i < m1; i++) {
-                grid[r][c] = grid[r + dx[0]][c + dy[0]];
-                r += dx[0];
-                c += dy[0];
-            }
-            for (int i = 0; i < m2; i++) {
-                grid[r][c] = grid[r + dx[1]][c + dy[1]];
-                r += dx[1];
-                c += dy[1];
-            }
-            for (int i = 0; i < m3; i++) {
-                grid[r][c] = grid[r + dx[2]][c + dy[2]];
-                r += dx[2];
-                c += dy[2];
-            }
-            for (int i = 0; i < m4 - 1; i++) {
-                grid[r][c] = grid[r + dx[3]][c + dy[3]];
-                r += dx[3];
-                c += dy[3];
-            }
-        }
-
-        grid[r][c] = temp;
+        grid[r - dx[3]][c - dy[3]] = temp;
     }
 }
