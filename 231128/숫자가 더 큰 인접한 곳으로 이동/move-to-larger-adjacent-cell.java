da7a76623f2692a1aee191b645f11820d0a57d7b
PKafka <66552990+PKafka0320@users.noreply.github.com>
2023-11-28 11:39:12 +0900

    Title: 숫자가 더 큰 인접한 곳으로 이동, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 231128/숫자가 더 큰 인접한 곳으로 이동/move-to-larger-adjacent-cell.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int r = sc.nextInt() - 1;
+        int c = sc.nextInt() - 1;
+
+        int[][] grid = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                grid[i][j] = sc.nextInt();
+            }
+        }
+
+        int[] dx = {-1, 1, 0, 0};
+        int[] dy = {0, 0, 1, -1};
+        boolean canMove = true;
+        int current = grid[r][c];
+        while (canMove) {
+            System.out.print(grid[r][c] + " ");
+            canMove = false;
+            for (int dir = 0; dir < 4; dir++) {
+                int nx = r + dx[dir];
+                int ny = c + dy[dir];
+                if (outOfRange(n, nx, ny)) continue;
+                if (grid[nx][ny] > grid[r][c]) {
+                    canMove = true;
+                    r = nx;
+                    c = ny;
+                    break;
+                }
+            }
+        }
+    }
+
+    public static boolean outOfRange(int n, int x, int y) {
+        return (x < 0 || x >= n || y < 0 || x >= n);
+    }
+}
