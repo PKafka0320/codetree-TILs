513a260ec0d91107b8ab7fac5c446030d7b2c7ca
PKafka <66552990+PKafka0320@users.noreply.github.com>
2023-12-03 13:29:37 +0900

    Title: 벽이 있는 충돌 실험, Time: 1764ms, Memory: 124MB, Status: Passed - CodeTree

--- 231203/벽이 있는 충돌 실험/collision-experiment-with-wall.java
+++ 231203/벽이 있는 충돌 실험/collision-experiment-with-wall.java
@@ -1,8 +1,16 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+
+class Marble {
+    int x, y, z;
+    public Marble(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+}
 
 public class Main {
-    public static final int COLLIDE = -2;
-    public static final int BLANK = -1;
     public static final int ASCII_NUM = 128;
     public static final int MAX_N = 50;
     public static final int DIR_NUM = 4;
@@ -10,8 +18,7 @@ public class Main {
     // 전역 변수 선언:
     public static int t, n, m;
     
-    public static int[][] currDir = new int[MAX_N + 1][MAX_N + 1];
-    public static int[][] nextDir = new int[MAX_N + 1][MAX_N + 1];
+    public static int[][] marbleCnt = new int[MAX_N + 1][MAX_N + 1];
     
     public static int[] mapper = new int[ASCII_NUM];
     
@@ -20,65 +27,77 @@ public class Main {
     public static int[] dx = new int[]{-1, 0, 0, 1};
     public static int[] dy = new int[]{0, 1, -1, 0};
     
+    public static ArrayList<Marble> marbles = new ArrayList<>();
+    
     // 해당 위치가 격자 안에 들어와 있는지 확인합니다.
     public static boolean inRange(int x, int y) {
         return 1 <= x && x <= n && 1 <= y && y <= n;
     }
     
-    // 해당 위치에 dir 방향을 갖는 구슬이 새롭게 추가되는 경우에 대한
-    // 처리를 합니다.
-    public static void updateNextDir(int x, int y, int dir) {
-        // 빈 곳이었다면 해당 구슬을 넣어주고
-        if(nextDir[x][y] == BLANK)
-            nextDir[x][y] = dir;
-        // 빈 곳이 아니었다면 이미 다른 구슬이 놓여져 있는 것이므로
-        // 충돌 표시를 해줍니다.
-        else
-            nextDir[x][y] = COLLIDE;
-    }
+    // 해당 구슬이 1초 후에 어떤 위치에서 어떤 방향을 보고 있는지를 구해
+    // 그 상태를 반환합니다.
+    public static Marble Move(Marble marble) {
+        // tuple의 경우 다음과 같이 원하는 변수에 값을 뽑아줄 수 있습니다.
+        int x = marble.x;
+        int y = marble.y;
+        int dir = marble.z;
     
-    public static void move(int x, int y, int dir) {
         // 바로 앞에 벽이 있는지 판단합니다.
         int nx = x + dx[dir], ny = y + dy[dir];
         
         // Case 1 : 벽이 없는 경우에는 그대로 한 칸 전진합니다.
-        // 따라서 그 다음 위치에 같은 방향을 갖는 구슬이 있게 됩니다.
         if(inRange(nx, ny))
-            updateNextDir(nx, ny, dir);
-    
+            return new Marble(nx, ny, dir);
         // Case 2 : 벽이 있는 경우에는 방향을 반대로 틀어줍니다.
-        // 따라서 같은 위치에 반대 방향을 갖는 구슬이 있게 됩니다.
+        // 처음에 dx, dy를 dir 기준 0, 3이 대칭 1, 2가 대칭이 되도록
+        // 설정해놨기 때문에 간단하게 처리가 가능합니다.
         else
-            updateNextDir(x, y, 3 - dir);
+            return new Marble(x, y, 3 - dir);
     }
     
     // 구슬을 전부 한 번씩 움직여 봅니다.
     public static void moveAll() {
-        // 그 다음 각 위치에서의 방향들을 전부 초기화해놓습니다.
-        for(int i = 1; i <= n; i++)
-            for(int j = 1; j <= n; j++)
-                nextDir[i][j] = BLANK;
-        
-        // (i, j) 위치에 구슬이 있는경우 
-        // 움직임을 시도해보고, 그 결과를 전부 nextDir에 기록합니다.
-        for(int i = 1; i <= n; i++)
-            for(int j = 1; j <= n; j++)
-                if(currDir[i][j] != BLANK)
-                    move(i, j, currDir[i][j]);
+        for(int i = 0; i < (int) marbles.size(); i++) {
+            Marble nextMarble = Move(marbles.get(i));
+            marbles.set(i, nextMarble);
+        }
+    }
     
-        // nextDir 값을 currDir에 복사합니다.
-        for(int i = 1; i <= n; i++)
-            for(int j = 1; j <= n; j++)
-                currDir[i][j] = nextDir[i][j];
+    // 해당 구슬과 충돌이 일어나는 구슬이 있는지 확인합니다.
+    // 이를 위해 자신의 현재 위치에 놓인 구슬의 개수가 
+    // 자신을 포함하여 2개 이상인지 확인합니다.
+    public static boolean duplicateMarbleExist(int targetIdx) {
+        int targetX = marbles.get(targetIdx).x;
+        int targetY = marbles.get(targetIdx).y;
+    
+        return marbleCnt[targetX][targetY] >= 2;
     }
     
     // 충돌이 일어나는 구슬은 전부 지워줍니다.
     public static void removeDuplicateMarbles() {
-        // 충돌이 일어난 구슬들이 있는 위치만 빈 곳으로 설정하면 됩니다.
-        for(int i = 1; i <= n; i++)
-            for(int j = 1; j <= n; j++)
-                if(currDir[i][j] == COLLIDE)
-                    currDir[i][j] = BLANK;
+        ArrayList<Marble> tempVector = new ArrayList<>();
+        
+        // Step2-1 : 각 구슬의 위치에 count를 증가시킵니다.
+        for(int i = 0; i < (int) marbles.size(); i++) {
+            int x = marbles.get(i).x;
+            int y = marbles.get(i).y;
+            marbleCnt[x][y]++;
+        }
+    
+        // Step2-2 : 충돌이 일어나지 않는 구슬만 전부 기록합니다.
+        for(int i = 0; i < (int) marbles.size(); i++)
+            if(!duplicateMarbleExist(i))
+                tempVector.add(marbles.get(i));
+    
+        // Step2-3 : 나중을 위해 각 구슬의 위치에 적어놓은 count 수를 다시 초기화합니다.
+        for(int i = 0; i < (int) marbles.size(); i++) {
+            int x = marbles.get(i).x;
+            int y = marbles.get(i).y;
+            marbleCnt[x][y]--;
+        }
+    
+        // step2-4 : 충돌이 일어나지 않은 구슬들로 다시 채워줍니다.
+        marbles = tempVector;
     }
     
     // 조건에 맞춰 시뮬레이션을 진행합니다.
@@ -103,20 +122,17 @@ public class Main {
         t = sc.nextInt();
 
         while(t-- > 0) {
+            // 새로운 테스트 케이스가 시작될때마다 기존에 사용하던 값들을 초기화해줍니다.
+            marbles = new ArrayList<>();
+
             // 입력:
             n = sc.nextInt();
             m = sc.nextInt();
-
-            // 새로운 테스트 케이스가 시작될때마다 기존에 사용하던 값들을 초기화해줍니다.
-            for(int i = 1; i <= n; i++)
-                for(int j = 1; j <= n; j++)
-                    currDir[i][j] = BLANK;
-
             for(int i = 1; i <= m; i++) {
                 int x = sc.nextInt();
                 int y = sc.nextInt();
                 char d = sc.next().charAt(0);
-                currDir[x][y] = mapper[d];
+                marbles.add(new Marble(x, y, mapper[d]));
             }
 
             // 2*n번 이후에는 충돌이 절대 일어날 수 없으므로
@@ -125,14 +141,7 @@ public class Main {
                 simulate();
             
             // 출력:
-            int marbleCnt = 0;
-
-            for(int i = 1; i <= n; i++)
-                for(int j = 1; j <= n; j++)
-                    if(currDir[i][j] != BLANK)
-                        marbleCnt++;
-
-            System.out.println(marbleCnt);
+            System.out.println((int) marbles.size());
         }
     }
 }
