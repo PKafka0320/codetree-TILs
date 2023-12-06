96aa210a3c4c4e27310f667cd13b4955cc4d128e
PKafka <66552990+PKafka0320@users.noreply.github.com>
2023-12-06 11:48:21 +0900

    Title: 벽이 없는 충돌 실험, Time: 673ms, Memory: 33MB, Status: Passed - CodeTree

--- 231206/벽이 없는 충돌 실험/collision-experiment-without-wall.java
+++ 231206/벽이 없는 충돌 실험/collision-experiment-without-wall.java
@@ -1,7 +1,8 @@
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.Collections;
 
-class Marble {
+class Marble implements Comparable<Marble> {
     int x, y, weight, dir, num;
     public Marble(int x, int y, int weight, int dir, int num) {
         this.x = x;
@@ -10,13 +11,46 @@ class Marble {
         this.dir = dir;
         this.num = num;
     }
+
+    // 구슬을 무게를 내림차순으로 정렬합니다.
+    // 무게가 동일할 경우 숫자를 내림차순으로 정렬하여
+    // 정렬 이후 더 앞선 구슬들이
+    // 충돌시에 항상 더 영향력을 가질 수 있도록 합니다.
+    @Override
+    public int compareTo(Marble b) {
+        if(weight != b.weight) return b.weight - weight;
+        return b.num - num;
+    }
+}
+
+class Collision implements Comparable<Collision> {
+    int time, index1, index2;
+    public Collision(int time, int index1, int index2) {
+        this.time = time;
+        this.index1 = index1;
+        this.index2 = index2;
+    }
+
+    // 시간을 오름차순으로 정렬합니다.
+    @Override
+    public int compareTo(Collision b) {
+        if(time != b.time) return time - b.time;
+        if(index1 != b.index1) return index1 - b.index1;
+        return index2 - b.index2;
+    }
+}
+
+class Pair { 
+    int x, y;
+    public Pair(int x, int y) { 
+        this.x = x; 
+        this.y = y; 
+    } 
 }
 
 public class Main {
-    public static final int BLANK = -1;
     public static final int ASCII_NUM = 128;
-    public static final int COORD_SIZE = 4000;
-    public static final int OFFSET = 2000;
+    public static final int MAX_N = 100;
     public static final int DIR_NUM = 4;
     
     // 전역 변수 선언:
@@ -27,124 +61,120 @@ public class Main {
     public static int[] dx = new int[]{0, 1, -1, 0};
     public static int[] dy = new int[]{1, 0, 0, -1};
     
-    public static int currTime;
+    public static boolean[] disappear = new boolean[MAX_N + 1];
+    
     public static int lastCollisionTime;
     
     public static ArrayList<Marble> marbles = new ArrayList<>();
-    public static ArrayList<Marble> nextMarbles = new ArrayList<>();
-    public static int[][] nextMarbleIndex = new int[COORD_SIZE + 1][COORD_SIZE + 1];
+    public static ArrayList<Collision> collisions = new ArrayList<>();
     
-    // 해당 구슬이 1초 후에 어디 위치에 있는지를 구해 상태를 반환합니다.
-    public static Marble Move(Marble marble) {
+    // 해당 구슬의 k초 후의 위치를 계산하여 반환합니다.
+    public static Pair Move(Marble marble, int k) {
         // tuple의 경우 다음과 같이 원하는 변수에 값을 뽑아줄 수 있습니다.
         int x = marble.x;
         int y = marble.y;
-        int weight = marble.weight;
         int dir = marble.dir;
-        int num = marble.num;
         
-        int nx = x + dx[dir], ny = y + dy[dir];
-        return new Marble(nx, ny, weight, dir, num);
+        int nx = x + dx[dir] * k, ny = y + dy[dir] * k;
+        return new Pair(nx, ny);
     }
     
-    // 해당 구슬과 충돌이 일어나는 구슬이 있는지 확인합니다.
-    // 있다면 해당 구슬의 index를 반환하고, 없다면 BLANK를 반환합니다.
-    // 이는 nextMarbleIndex를 활용하면 O(1)에 바로 가능합니다.
-    public static int findDuplicateMarble(Marble marble) {
-        int targetX = marble.x;
-        int targetY = marble.y;
+    // 두 구슬만 좌표 평면 위에 존재한다 했을 때
+    // 충돌이 일어난다면 언제 일어나는지 그 시간을 반환합니다.
+    // 만약 충돌이 일어나지 않는다면 -1을 반환합니다.
+    public static int collisionOccurTime(Marble marble1, Marble marble2) {
+        int x1 = marble1.x;
+        int y1 = marble1.y;
+        int dir1 = marble1.dir;
+
+        int x2 = marble2.x;
+        int y2 = marble2.y;
+        int dir2 = marble2.dir;
     
-        return nextMarbleIndex[targetX][targetY];
-    }
+        // Case1 : 두 구슬의 방향이 같은 경우에는 절대 충돌하지 않습니다.
+        if(dir1 == dir2)
+            return -1;
     
-    // 두 구슬이 같은 위치에서 충돌했을 경우
-    // 살아남는 구슬을 반환합니다.
-    public static Marble Collide(Marble marble1, Marble marble2) {
-        int weight1 = marble1.weight;
-        int num1 = marble1.num;
+        // Case2 : 두 구슬의 방향이 반대인 경우에는 
+        //         x, y 값 중 하나가 일치해야 하고
+        //         두 구슬의 거리를 반으로 나눈 값 만큼
+        //         두 구슬을 각각의 방향으로 움직였을 때 
+        //         서로 같은 위치로 도달해야 충돌한다고 볼 수 있습니다. 
+        if(dir1 == 3 - dir2) {
+            // x, y 둘 다 일치하지 않으면 불가합니다.
+            if(x1 != x2 && y1 != y2)
+                return -1;
+            
+            // x, y 둘 중에 하나가 일치한다면 
+            // 처음에 모든 좌표를 다 2배씩 해줬기 때문에 
+            // dist는 짝수임을 보장할 수 있습니다. 
+            int dist = (x1 != x2) ? Math.abs(x1 - x2) : Math.abs(y1 - y2);
+            int half = dist / 2;
+            
+            Pair nextPos1 = Move(marble1, half);
+            Pair nextPos2 = Move(marble2, half);
+            if(nextPos1.x == nextPos2.x && nextPos1.y == nextPos2.y)
+                return half;
+            else
+                return -1;
+        }
     
-        int weight2 = marble2.weight;
-        int num2 = marble2.num;
+        // Case3 : 두 방향이 서로 나란히 있지 않은 경우에는
+        //         두 구슬의 x좌표, y좌표의 차이가 정확히 일치해야 하며
+        //         두 구슬의 각각의 방향으로 그 거리의 차이 만큼씩 움직였을 때
+        //         서로 같은 위치로 도달해야 충돌한다고 볼 수 있습니다. 
     
-        // 첫 번째 구슬을 따라가게 되는 경우는
-        // 첫 번째 구슬의 무게가 더 크거나
-        // 무게는 같은데 번호가 더 클 경우 입니다.
-        if(weight1 > weight2 || (weight1 == weight2 && num1 > num2))
-            return marble1;
+        int xDist = Math.abs(x1 - x2);
+        int yDist = Math.abs(y1 - y2);
+        Pair nextPos1 = Move(marble1, xDist);
+        Pair nextPos2 = Move(marble2, xDist);
+        if(xDist == yDist && nextPos1.x == nextPos2.x && nextPos1.y == nextPos2.y)
+            return xDist;
         else
-            return marble2;
+            return -1;
     }
     
-    // 구슬이 이미 (0, 0) ~ (COORD_SIZE, COORD_SIZE) 사이를 벗어났다면
-    // 더 이상 충돌이 일어나지 않으므로
-    // Active Coordinate를 벗어났다고 판단합니다.
-    public static boolean outOfActiveCoordinate(Marble marble) {
-        int x = marble.x;
-        int y = marble.y;
-    
-        return x < 0 || x > COORD_SIZE || y < 0 || y > COORD_SIZE;
-    }
-    
-    // 그 다음 구슬의 목록에 반영합니다.
-    public static void pushNextMarble(Marble marble) {
-        // 구슬이 이미 (0, 0) ~ (COORD_SIZE, COORD_SIZE) 사이를 벗어났다면
-        // 그 이후부터는 절대 충돌이 일어나지 않으므로
-        // 그 구슬은 앞으로 더 이상 관찰하지 않습니다. 
-        if(outOfActiveCoordinate(marble))
-            return;
-    
-        int index = findDuplicateMarble(marble);
-    
-        // Case1 : 같은 위치에 있는 구슬이 아직 없다면 그대로 목록에 추가합니다.
-        if(index == BLANK) {
-            nextMarbles.add(marble);
-    
-            // 나중에 위치가 겹치는 구슬이 있는지,
-            // 만약 있다면 그 구슬이 nextMarbles의 어느 index에 있는지를
-            // 상수 시간안에 판단하기 위해 해당 위치에
-            // 새로 추가되는 구슬의 index를 적어놓습니다.
-            int x = marble.x;
-            int y = marble.y;
-            nextMarbleIndex[x][y] = (int) (nextMarbles.size()) - 1;
-        }
-    
-        // Case2 :
-        // 다음 구슬의 목록 중 같은 위치에 구슬이 이미 있다면
-        // 더 영향력 있는 구슬만 남기고
-        // 현재 시간을 가장 최근 충돌 시간에 기록합니다.
-        else {
-            Marble newMarble = Collide(nextMarbles.get(index), marble);
-            nextMarbles.set(index, newMarble);
-            lastCollisionTime = currTime;
-        }
-    }
-    
-    // 모든 구슬들을 한 칸씩 움직이는 시뮬레이션을 진행합니다.
-    public static void simulate() {
-        for(int i = 0; i < (int) marbles.size(); i++) {
-            // Step1 : 각 구슬에 대해 한 칸 움직인 이후의 위치를 받아옵니다.
-            Marble nextMarble = Move(marbles.get(i));
-    
-            // Step2 : 그 다음 구슬의 목록에 반영합니다.
-            pushNextMarble(nextMarble);
-        }
+    // 모든 구슬쌍에 대해 충돌이 일어나는지 확인하고
+    // 발생 가능한 충돌들에 대해 시간순으로 정렬해줍니다.
+    public static void arrangeCollisions() {
+        for(int i = 0; i < (int) marbles.size(); i++)
+            for(int j = i + 1; j < (int) marbles.size(); j++) {
+                int time = collisionOccurTime(marbles.get(i), marbles.get(j));
+                if(time != -1)
+                    collisions.add(new Collision(time, i, j));
+            }
         
-        marbles = (ArrayList<Marble>) nextMarbles.clone();
+         // 시간순으로 오름차순으로 정렬합니다.
+        Collections.sort(collisions);
+    }
     
-        // 그 다음 Simulation 때 다시 사용해야하므로
-        // 충돌여부를 빠르게 판단하기 위해 쓰였던 nextMarbleIndex 배열과
-        // 다음 구슬의 목록을 기록했던 nextMarbles를 미리 초기화해줍니다. 
+    // 시간에 따라 충돌 단위로 시뮬레이션을 진행합니다.
+    public static void simulate() {
+        for(int i = 0; i < (int) collisions.size(); i++) {
+            int collisionTime = collisions.get(i).time;
+            int index1 = collisions.get(i).index1;
+            int index2 = collisions.get(i).index2;
+            
+            // 두 구슬 중 하나라도 이미 이전의 충돌로 인해 소멸되어 버렸다면
+            // 두 구슬은 실제로 충돌이 일어날 수 없었다는 의미이므로
+            // 패스합니다.
+            if(disappear[index1] || disappear[index2])
+                continue;
     
-        for(int i = 0; i < (int) nextMarbles.size(); i++) {
-            int x = nextMarbles.get(i).x;
-            int y = nextMarbles.get(i).y;
-            nextMarbleIndex[x][y] = BLANK;
+            // 처음에 구슬의 목록을 (무게 순, 번호가 더 큰 순)으로
+            // 정렬해놨기 때문에 index1 < index2인 경우 
+            // 항상 index1이 더 영향력이 크기 때문에 살아남게 되고
+            // index2는 소멸하게 됩니다.
+            disappear[index2] = true;
+            lastCollisionTime = collisionTime;
         }
-        nextMarbles = new ArrayList<>();
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        // 3 - dir 방향이 dir 방향과 정 반대가 되도록
+        // dir에 따른 dx, dy 값을 적절하게 정의합니다.
+        // 후에 두 구슬의 방향이 서로 정 반대인지 쉽게 판단하기 위함입니다. 
         mapper['U'] = 0;
         mapper['R'] = 1;
         mapper['L'] = 2;
@@ -153,15 +183,14 @@ public class Main {
         // 테스트 케이스 수 입력:
         t = sc.nextInt();
 
-        // 처음에는 구슬이 전혀 놓여있지 않다는 표시로 전부 BLANK로 채워 놓습니다.
-        for(int i = 0; i <= COORD_SIZE; i++)
-             for(int j = 0; j <= COORD_SIZE; j++)
-                nextMarbleIndex[i][j] = BLANK;
-
         while(t-- > 0) {
             // 새로운 테스트 케이스가 시작될때마다 기존에 사용하던 값들을 초기화해줍니다.
             marbles = new ArrayList<>();
+            collisions = new ArrayList<>();
             lastCollisionTime = -1;
+
+            for(int i = 1; i <= MAX_N; i++)
+                disappear[i] = false;
 
             // 입력:
             n = sc.nextInt();
@@ -177,28 +206,20 @@ public class Main {
                 // 않아도 됩니다.
                 x *= 2; y *= 2;
 
-                // 좌표를 전부 양수로 만들어야 동일한 위치에서 충돌이 일어나는지를
-                // 판단하는 데 사용할 nextMarbleIndex 배열에
-                // 각 구슬의 위치마다 자신의 index를 저장할 수 있으므로
-                // 좌표를 전부 양수로 만듭니다.
-                // 입력으로 들어올 수 있는 좌표값 중 가장 작은 값이 -2000 이므로
-                // OFFSET을 2000으로 잡아 전부 더해줍니다.
-                // 같은 OFFSET을 모든 구슬에 전부 더해주는 것은
-                // 답에 전혀 영향을 미치지 않습니다.
-                x += OFFSET; y += OFFSET;
                 marbles.add(new Marble(x, y, weight, mapper[d], i));
             }
 
-            // OFFSET이 더해진 구슬들의 처음 위치는 전부 
-            // (0, 0)에서 (4000, 4000) 사이에 있기 때문에 
-            // COORD SIZE + 1(4001)만큼 이동하면
-            // 입력으로 주어진 구슬들이 모두 (0, 0) ~ (4000, 4000)
-            // 영역 밖으로 벗어나게 되므로 더 이상 충돌이 일어나지 않게 됩니다.
-            // 따라서 시뮬레이션을 총 COORD_SIZE번 진행합니다.
-            for(int i = 1; i <= COORD_SIZE; i++) {
-                currTime = i;
-                simulate();
-            }
+            // 충돌시 영향력이 더 높은 구슬이 앞으로 오도록 정렬합니다.
+            // 영향력이 더 높다 함은 무게가 더 크거나, 무게가 같더라도 번호가 더 커
+            // 충돌시 살아남게 되는 구슬을 의미합니다.
+            Collections.sort(marbles);
+            
+            // 모든 구슬쌍에 대해 충돌이 일어나는 경우를 구해
+            // 시간순으로 정리해줍니다.
+            arrangeCollisions();
+
+            // 시간에 따라 충돌 단위로 시뮬레이션을 진행합니다.
+            simulate();
             
             // 출력:
             System.out.println(lastCollisionTime);
