1fcf7a7fb1e715eaba9099888709e3825c6e927d
PKafka <66552990+PKafka0320@users.noreply.github.com>
2023-12-08 10:57:56 +0900

    Title: n개의 점 중 m개 고르기, Time: 184ms, Memory: 11MB, Status: Passed - CodeTree

--- 231208/n개의 점 중 m개 고르기/choose-m-out-of-n-points.java
+++ 231208/n개의 점 중 m개 고르기/choose-m-out-of-n-points.java
@@ -1,80 +1,82 @@
-import java.util.*;
+import java.util.Scanner;
+import java.util.ArrayList;
 
-class Pair {
+class Pair { 
     int x, y;
-    public Pair(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
+    public Pair(int x, int y) { 
+        this.x = x; 
+        this.y = y; 
+    } 
 }
 
 public class Main {
-    static int N, M;
-    static ArrayList<Pair> pairs = new ArrayList<>();
-    static ArrayList<Pair> selected = new ArrayList<>();
-    static int minDist = Integer.MAX_VALUE;
+    public static final int INT_MAX = Integer.MAX_VALUE;
+    public static final int MAX_N = 20;
+    
+    public static int n, m;
+    public static Pair[] points = new Pair[MAX_N];
+    
+    public static ArrayList<Pair> selectedPoints = new ArrayList<>();
+    
+    public static int ans = INT_MAX;
+    
+    public static int dist(Pair p1, Pair p2) {
+        int x1 = p1.x;
+        int y1 = p1.y;
+
+        int x2 = p2.x;
+        int y2 = p2.y;
+        
+        return (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2);
+    }
+    
+    public static int calc() {
+        int maxDist = 0;
+        
+        // 가장 먼 거리를 반환합니다.
+        for(int i = 0; i < m; i++) 
+            for(int j = i + 1; j < m; j++) 
+                maxDist = Math.max(maxDist, 
+                               dist(selectedPoints.get(i), selectedPoints.get(j)));
+            
+        return maxDist;
+    }
+    
+    public static void findMin(int idx, int cnt) {
+        if(cnt == m) {
+            // 가장 먼 거리 중 최솟값을 선택합니다.
+            ans = Math.min(ans, calc());
+            return;
+        }
+        
+        if(idx == n) 
+            return;
+        
+        // 점을 선택하는 경우입니다.
+        selectedPoints.add(points[idx]);
+        findMin(idx + 1, cnt + 1);
+        selectedPoints.remove(selectedPoints.size() - 1);
+        
+        // 점을 선택하지 않는 경우입니다.
+        findMin(idx + 1, cnt);
+    }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        M = sc.nextInt();
-        for (int i = 0; i < N; i++) {
+        n = sc.nextInt();
+        m = sc.nextInt();
+
+        for(int i = 0; i < n; i++)
+            points[i] = new Pair(0, 0);
+        
+        for(int i = 0; i < n; i++) {
             int x = sc.nextInt();
             int y = sc.nextInt();
-
-            pairs.add(new Pair(x, y));
+            points[i] = new Pair(x, y);
         }
-
-        select(0, 0);
-
-        System.out.println(minDist);
-    }
-
-    public static void select(int index, int count) {
-        if (count >= M) {
-            findDist();
-            return;
-        }
-        if (index >= N) {
-            return;
-        }
-        selected.add(pairs.get(index));
-        select(index + 1, count + 1);
-        selected.remove(selected.size() - 1);
-        select(index + 1, count);
-    }
-
-    public static void findDist() {
-        int maxDist = 0;
-        int x1 = 0;
-        int y1 = 0;
-        int x2 = 0;
-        int y2 = 0;
-
-        for (int fromIndex = 0; fromIndex < selected.size() - 1; fromIndex++) {
-            Pair from = selected.get(fromIndex);
-            int fx = from.x;
-            int fy = from.y;
-
-            for (int toIndex = fromIndex + 1; toIndex < selected.size(); toIndex++) {
-                Pair to = selected.get(toIndex);
-                int tx = to.x;
-                int ty = to.y;
-
-                int dist = (Math.abs(fx - tx) * Math.abs(fx - tx)) + (Math.abs(fy - ty) * Math.abs(fy - ty));
-                if (dist > maxDist) {
-                    maxDist = dist;
-                    x1 = fx;
-                    y1 = fy;
-                    x2 = tx;
-                    y2 = ty;
-                }
-            }
-        }
-
-        int finalDist = (Math.abs(x1 - x2) * Math.abs(x1 - x2)) + (Math.abs(y1 - y2) * Math.abs(y1 - y2));
-        if (finalDist < minDist) {
-            minDist = finalDist;
-        }
+        
+        findMin(0, 0);
+        
+        System.out.print(ans);
     }
 }
