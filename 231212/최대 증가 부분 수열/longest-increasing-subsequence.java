edcb461c60ebe7e2d37014da72d34ef088ebb0fb
PKafka <66552990+PKafka0320@users.noreply.github.com>
2023-12-12 11:16:00 +0900

    Title: 최대 증가 부분 수열, Time: 79ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 231212/최대 증가 부분 수열/longest-increasing-subsequence.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static BufferedReader br;
+    static StringTokenizer st;
+    public static void main(String[] args) {
+        
+    }
+}
