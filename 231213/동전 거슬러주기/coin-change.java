dc56af8a89c575036e86ed94009e6d9ee4febdcd
PKafka <66552990+PKafka0320@users.noreply.github.com>
2023-12-13 15:26:39 +0900

    Title: 동전 거슬러주기, Time: 85ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 231213/동전 거슬러주기/coin-change.java
@@ -0,0 +1,41 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static BufferedReader br;
+    static StringTokenizer st;
+    static int N;
+    static int M;
+    static int[] coins;
+    static int[] minCount;
+
+    public static void main(String[] args) throws Exception {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        coins = new int[N];
+        minCount = new int[M + 1];
+        st = new StringTokenizer(br.readLine());
+        for (int idx = 0; idx < N; idx++) {
+            coins[idx] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int number = 1; number < M + 1; number++) {
+            for (int coinIdx = 0; coinIdx < N; coinIdx++) {
+                if (number - coins[coinIdx] < 0) continue;
+                int tempCount = minCount[number - coins[coinIdx]] + 1;
+                if (minCount[number] == 0 || minCount[number] > tempCount) {
+                    minCount[number] = tempCount;
+                }
+            }
+        }
+
+        if (minCount[M] == 0) {
+            System.out.println(-1);
+        } else {
+            System.out.println(minCount[M]);
+        }
+    }
+}
