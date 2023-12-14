6ef993233cf8bcad02284062528ded2b611647b9
PKafka <66552990+PKafka0320@users.noreply.github.com>
2023-12-14 17:18:30 +0900

    Title: 최대 동전 거슬러주기, Time: 121ms, Memory: 9MB, Status: Passed - CodeTree

--- 231214/최대 동전 거슬러주기/max-coin-change.java
+++ 231214/최대 동전 거슬러주기/max-coin-change.java
@@ -24,9 +24,14 @@ public class Main {
         for (int number = 1; number <= M; number++) {
             for (int idx = 0; idx < N; idx++) {
                 if (number < coins[idx]) continue;
+                if (maxCount[number - coins[idx]] == 0 && number != coins[idx]) continue;
                 maxCount[number] = Math.max(maxCount[number], maxCount[number - coins[idx]] + 1);
             }
         }
+
+        // for (int number = 0; number <= M; number++) {
+        //     System.out.println(number + " : " + maxCount[number]);
+        // }
 
         int answer = maxCount[M];
         if (answer == 0) {
