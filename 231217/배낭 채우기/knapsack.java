a7b707404755ad2faa16649b453f8aee0eadcc97
PKafka <66552990+PKafka0320@users.noreply.github.com>
2023-12-17 12:49:53 +0900

    Title: 배낭 채우기, Time: 87ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 231217/배낭 채우기/knapsack.java
@@ -0,0 +1,49 @@
+import java.util.*;
+import java.io.*;
+
+class Jewel {
+    int weight, value;
+    public Jewel(int w, int v) {
+        this.weight = w;
+        this.value = v;
+    }
+}
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static ArrayList<Jewel> jewels = new ArrayList<>();
+    static int[] maxValue;
+    static int N, M;
+    static int answer = 0;
+
+    public static void main(String[] args) throws Exception {
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        maxValue = new int[M + 1];
+
+        for (int idx = 0; idx < N; idx++) {
+            st = new StringTokenizer(br.readLine());
+            int w = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            jewels.add(new Jewel(w, v));
+        }
+
+        for (int idx = 0; idx < N; idx++) {
+            Jewel jewel = jewels.get(idx);
+
+            for (int weight = M; weight >= 1; weight--) {
+                if (weight < jewel.weight) continue;
+                if (weight == jewel.weight) {
+                    maxValue[weight] = jewel.value;
+                }
+                if (maxValue[weight - jewel.weight] == 0) continue;
+                maxValue[weight] = Math.max(maxValue[weight], maxValue[weight - jewel.weight] + jewel.value);
+                if (answer < maxValue[weight]) answer = maxValue[weight];
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
