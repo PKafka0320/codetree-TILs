be5472f994128ccbaca3fafe7843ecf427ea71e6
PKafka <66552990+PKafka0320@users.noreply.github.com>
2023-12-21 10:20:49 +0900

    Title: 정수 사각형 최솟값의 최대, Time: 320ms, Memory: 31MB, Status: Passed - CodeTree

--- 231221/정수 사각형 최솟값의 최대/maximin-path-in-square.java
+++ 231221/정수 사각형 최솟값의 최대/maximin-path-in-square.java
@@ -1,38 +1,63 @@
-import java.io.*;
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static StringTokenizer st;
-    static int n;
-    static int[][] grid;
-    static int[][] min;
-
-    public static void main(String[] args) throws Exception {
-        n = Integer.parseInt(br.readLine());
-        grid = new int[n][n];
-        min = new int[n][n];
-
-        for (int row = 0; row < n; row++) {
-            st = new StringTokenizer(br.readLine());
-            for (int col = 0; col < n; col++) {
-                grid[row][col] = Integer.parseInt(st.nextToken());
-            }
+    public static final int UNUSED = -1;
+    public static final int DIR_NUM = 2;
+    public static final int MAX_NUM = 1000;
+    
+    public static int n;
+    public static int[][] grid = new int[MAX_NUM][MAX_NUM];
+    public static int[][] memo = new int[MAX_NUM][MAX_NUM];
+    
+    public static boolean inRange(int x, int y) {
+        return (0 <= x && x < n && 0 <= y && y < n);
+    }
+    
+    public static void initializeMemo() {
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < n; j++)
+                memo[i][j] = UNUSED;
+    }
+    
+    public static int findMaximin(int x, int y) {
+        // 미리 계산된 적이 있는 경우 해당 값을 사용해줍니다.
+        if(memo[x][y] != UNUSED)
+            return memo[x][y];
+    
+        // 도착 지점에 도착하면 최솟값의 최대를 갱신해줍니다.
+        if( (x == n - 1) &&  (y == n - 1) )
+            return memo[n - 1][n - 1] = grid[n - 1][n - 1] ;
+    
+        int[] dx = new int[]{1, 0};
+        int[] dy = new int[]{0, 1};
+    
+        // 가능한 방향에 대해 탐색해줍니다.
+        int maximin = 0;
+        for(int i = 0; i < DIR_NUM; i++) {
+            int newX = x + dx[i];
+            int newY = y + dy[i];
+    
+            if(inRange(newX, newY))
+                maximin = Math.max(maximin,
+                              Math.min(findMaximin(newX, newY), grid[x][y]));
         }
+    
+        // 게산된 값을 memo 배열에 저장해줍니다.
+        memo[x][y] = maximin;
+    
+        return maximin;
+    }
 
-        min[0][0] = grid[0][0];
-        for (int idx = 1; idx < n; idx++) {
-            min[0][idx] = Math.min(min[0][idx - 1], grid[0][idx]);
-            min[idx][0] = Math.min(min[idx - 1][0], grid[idx][0]);
-        }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
 
-        for (int row = 1; row < n; row++) {
-            for (int col = 1; col < n; col++) {
-                int minVal = Math.max(min[row - 1][col], min[row][col - 1]);
-                min[row][col] = Math.min(minVal, grid[row][col]);
-            }
-        }
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < n; j++)
+                grid[i][j] = sc.nextInt();
 
-        System.out.println(min[n - 1][n - 1]);
+        initializeMemo();
+
+        System.out.print(findMaximin(0, 0));
     }
 }
