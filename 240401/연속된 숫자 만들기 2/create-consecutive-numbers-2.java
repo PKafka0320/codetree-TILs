b05c81ee24902eb09fd0a1b3a2367ef573577948
PKafka <66552990+PKafka0320@users.noreply.github.com>
2024-04-01 14:25:48 +0900

    Title: 연속된 숫자 만들기 2, Time: 135ms, Memory: 10MB, Status: Passed - CodeTree

--- 240401/연속된 숫자 만들기 2/create-consecutive-numbers-2.java
+++ 240401/연속된 숫자 만들기 2/create-consecutive-numbers-2.java
@@ -1,57 +1,23 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int c = sc.nextInt();
 
-        int min = Math.min(Math.min(a, b), c);
-        int max = Math.max(Math.max(a, b), c);
-        int mid;
-        if (min == a || max == a) {
-            if (min == b || max == b) {
-                mid = c;
-            }
-            else mid = b;
-        }
-        else mid = a;
+        int[] arr = new int[3];
 
-        a = min;
-        b = mid;
-        c = max;
-        
-        int cnt = 0;
-        while (Math.max(Math.max(a, b), c) - Math.min(Math.min(a, b), c) > 2) {
-            int diff1 = b - a;
-            int diff2 = c - b;
-
-            if (diff1 > 2 && diff2 > 2) {
-                a = b;
-                b += 2;
-            }
-            else if (diff1 == 2) {
-                c = b;
-                b -= 1;
-            }
-            else if (diff2 == 2) {
-                a = b;
-                b += 1;
-            }
-            else if (diff1 == 1) {
-                a = b;
-                b += 2;
-            }
-            else if (diff2 == 1) {
-                c = b;
-                b -= 2;
-            }
-
-            // System.out.println(a + ", " + b + ", " + c);
-            cnt++;
+        for (int i = 0; i < 3; i++) {
+            arr[i] = sc.nextInt();
         }
 
-        System.out.println(cnt);
+        Arrays.sort(arr);
+
+        if(arr[0] + 1 == arr[1] && arr[1] + 1 == arr[2])
+            System.out.print(0);
+        else if(arr[0] + 2 == arr[1] || arr[1] + 2 == arr[2])
+            System.out.print(1);
+        else
+            System.out.print(2);
     }
 }
