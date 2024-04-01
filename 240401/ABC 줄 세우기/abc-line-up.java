7960acc438cc218e3a9ff5f90d02a9a32d29ecc6
PKafka <66552990+PKafka0320@users.noreply.github.com>
2024-04-01 15:32:32 +0900

    Title: ABC 줄 세우기, Time: 150ms, Memory: 10MB, Status: Passed - CodeTree

--- 240401/ABC 줄 세우기/abc-line-up.java
+++ 240401/ABC 줄 세우기/abc-line-up.java
@@ -12,18 +12,14 @@ public class Main {
 
         int cnt = 0;
         for (int i = 0; i < n; i++) {
-            if (arr[i] == i) continue;
-
-            int from = i;
-            int to = arr[i];
-            for (int j = from; j < to; j++) {
-                int tmp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = tmp;
-                cnt++;
+            for (int j = 0; j < n - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int tmp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = tmp;
+                    cnt++;
+                }
             }
-            i = 0;
-
         }
 
         System.out.println(cnt);
