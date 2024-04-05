879e901fdd509a12aebf081c3a9b4662e206e2a5
PKafka <66552990+PKafka0320@users.noreply.github.com>
2024-04-05 19:19:23 +0900

    Title: 계단 오르기 2, Time: 104ms, Memory: 9MB, Status: Passed - CodeTree

--- 240405/계단 오르기 2/climbing-stairs-2.java
+++ 240405/계단 오르기 2/climbing-stairs-2.java
@@ -1,17 +1,24 @@
 import java.util.Scanner;
+import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 
 public class Main {
-    static Scanner sc = new Scanner(System.in);
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
     static int n;
     static int[] coin;
     static int[][] maxCoin;
 
-    public static void main(String[] args) {
-        n = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        n = Integer.parseInt(br.readLine());
         coin = new int[n + 1];
         maxCoin = new int[n + 1][4];
+
+        st = new StringTokenizer(br.readLine());
         for (int i = 1; i <= n; i++) {
-            coin[i] = sc.nextInt();
+            coin[i] = Integer.parseInt(st.nextToken());
         }
 
         // init
