93619b3b68a85e96f93d07f35669cee6dc1d503c
PKafka <66552990+PKafka0320@users.noreply.github.com>
2024-04-09 12:39:08 +0900

    Title: 게임 스튜디오에서 살아남기, Time: 136ms, Memory: 10MB, Status: Passed - CodeTree

--- 240409/게임 스튜디오에서 살아남기/surviving-in-a-game-studio.java
+++ 240409/게임 스튜디오에서 살아남기/surviving-in-a-game-studio.java
@@ -3,30 +3,26 @@ import java.util.Scanner;
 public class Main {
     static Scanner sc = new Scanner(System.in);
     static int n;
-    static long[][][] dp;
+    static long[][][] dp; //i 번째 날까지 받은 T의 횟수와 연속된 B의 횟수가 가능한 경우의 수
 
     public static void main(String[] args) {
         n = sc.nextInt();
-        dp = new long[n + 1][3][3];
+        dp = new long[n + 1][4][4];
 
-        dp[1][0][0] = 1;
-        dp[1][1][0] = 1;
-        dp[1][0][1] = 1;
+        dp[1][0][0] = 1; //첫 번째 날에 G를 받은 경우
+        dp[1][1][0] = 1; //첫 번째 날에 T를 받은 경우
+        dp[1][0][1] = 1; //첫 번째 날에 B를 받은 경우
 
         int mod = 1000000007;
 
-        for (int i = 2; i <= n; i++) {
-            dp[i][0][0] = (dp[i - 1][0][0] + dp[i - 1][0][1] + dp[i - 1][0][2]) % mod;
-            dp[i][0][1] = dp[i - 1][0][0];
-            dp[i][0][2] = dp[i - 1][0][1];
-            dp[i][1][0] = (dp[i - 1][0][0] + dp[i - 1][0][1] + dp[i - 1][0][2] 
-                + dp[i - 1][1][0] + dp[i - 1][1][1] + dp[i - 1][1][2]) % mod;
-            dp[i][1][1] = dp[i - 1][1][0];
-            dp[i][1][2] = dp[i - 1][1][1];
-            dp[i][2][0] = (dp[i - 1][1][0] + dp[i - 1][1][1] + dp[i - 1][1][2] 
-                + dp[i - 1][2][0] + dp[i - 1][2][1] + dp[i - 1][2][2]) % mod;
-            dp[i][2][1] = dp[i - 1][2][0];
-            dp[i][2][2] = dp[i - 1][2][1];
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < 3; j++) {
+                for (int k = 0; k < 3; k++) {
+                    dp[i + 1][j + 1][0] = (dp[i + 1][j + 1][0] + dp[i][j][k]) % mod; //T를 받는 경우
+                    dp[i + 1][j][0] = (dp[i + 1][j][0] + dp[i][j][k]) % mod; //G를 받는 경우
+                    dp[i + 1][j][k + 1] = (dp[i + 1][j][k + 1] + dp[i][j][k]) % mod; //B를 받는 경우
+                }
+            }
         }
 
         long ans = 0;
