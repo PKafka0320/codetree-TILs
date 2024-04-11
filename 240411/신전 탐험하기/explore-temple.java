0c02f238e1533a73be64b922c0485bcab86307c9
PKafka <66552990+PKafka0320@users.noreply.github.com>
2024-04-11 11:22:12 +0900

    Title: 신전 탐험하기, Time: 218ms, Memory: 15MB, Status: Passed - CodeTree

--- 240411/신전 탐험하기/explore-temple.java
+++ 240411/신전 탐험하기/explore-temple.java
@@ -15,20 +15,21 @@ public class Main {
             }
         }
 
-        //dp[i][j] :: i 번째 층에서 j번째 방을 선택했을때 최대로 얻는 보물의 수
-        //j :: 0 = l, 1 = m, 2 = r
+        // dp[i][j] : i번째 층까지 올라왔을 때,
+        // j번 방 (j = 1일 때 : 왼쪽, j = 2일 때 : 가운데, j = 3일 때 : 오른쪽)
+        // 을 골랐을 때 가져갈 수 있는 보물의 최대 개수
         dp = new int[n + 1][3];
         
-        for (int i = 0; i < n; i++) { //i 번째 층
-            for (int j = 0; j < 3; j++) { //j 번째 방을 선택한 경우
-                for (int k = 0; k < 3; k++) { //이전 방이 k 번째인 경우
+        for (int i = 0; i < n; i++) { // i 번째 층
+            for (int j = 0; j < 3; j++) { // j 번째 방을 선택한 경우
+                for (int k = 0; k < 3; k++) { // 이전 방이 k 번째인 경우
                     if (j == k) continue;
                     dp[i + 1][j] = Math.max(dp[i + 1][j], dp[i][k] + treasuer[i + 1][j]);
                 }
             }
         }
 
-        //결과 계산
+        // 결과 계산
         int ans = 0;
         for (int i = 0; i < 3; i++) {
             ans = Math.max(ans, dp[n][i]);
