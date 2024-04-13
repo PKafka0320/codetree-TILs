78e0201aa19a4d3a72bc931b504d99b2787d2364
PKafka <66552990+PKafka0320@users.noreply.github.com>
2024-04-13 13:23:17 +0900

    Title: 적절하게 숫자를 변경하기, Time: 242ms, Memory: 15MB, Status: Passed - CodeTree

--- 240413/적절하게 숫자를 변경하기/change-number-properly.java
+++ 240413/적절하게 숫자를 변경하기/change-number-properly.java
@@ -1,66 +1,99 @@
 import java.util.Scanner;
-import java.util.Arrays;
 
 public class Main {
-    static Scanner sc = new Scanner(System.in);
-    static int n;
-    static int m;
-    static int[] num;
-    static int[][] dp;
+    public static final int INT_MIN = Integer.MIN_VALUE;
+    public static final int MAX_K = 4;
+    public static final int MAX_M = 100;
+    public static final int MAX_N = 500;
+    
+    // dp[i][j][k] :
+    // i번째 숫자까지 고려했고
+    // 그 동안 숫자를 j번 변경했고
+    // 마지막으로 사용한 숫자가 k라 했을 때 (k는 1, 2, 3, 4중에 하나)
+    // 얻을 수 있는 최대 유사도
+    public static int[][][] dp = new int[MAX_N + 1][MAX_M + 1][MAX_K + 1];
+    
+    public static int n, m;
+    
+    public static int[] a = new int[MAX_N + 1];
+    
+    public static void initialize() {
+        // 최댓값을 구하는 문제이므로, 
+        // 초기에는 전부 INT_MIN을 넣어줍니다.
+        for(int i = 1; i <= n; i++)
+            for(int j = 0; j <= m; j++)
+                for(int k = 1; k <= MAX_K; k++)
+                    dp[i][j][k] = INT_MIN;
+        
+        // 첫 번째 숫자가 k인 경우를 전부 초기 조건으로 설정해줍니다.
+        // 첫 번재 숫자가 k인 경우
+        // 첫 번째 숫자까지 그 동안 숫자를 0번 변경했고 마지막으로 적은 숫자가 k이며,
+        // 얻을 수 있는 유사도는 a[1]=k인 경우에는 1, 아닌 경우에는 0이 되어야 하므로
+        // dp[1][0][k] 에 알맞는 값을 설정해줍니다.
+        for(int k = 1; k <= MAX_K; k++)
+            dp[1][0][k] = (a[1] == k ? 1 : 0);
+    }
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
         m = sc.nextInt();
-        
-        // num[i] : i 번째 숫자
-        num = new int[n];
-        for (int i = 0; i < n; i++) {
-            num[i] = sc.nextInt();
-        }
 
-        // dp[i][j] : i 번째 숫자까지 고려하고, 숫자가 변경된 횟수가 j 번 일때의 최대 유사도
-        dp = new int[n][m + 1];
-        for (int i = 0; i < n; i++) {
-            Arrays.fill(dp[i], -1);
-        }
-        dp[0][0] = 1;
+        for(int i = 1; i <= n; i++)
+            a[i] = sc.nextInt();
 
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j <= m; j++) {
-                if (dp[i][j] == -1) continue;
+        initialize();
 
-                // 숫자 변경이 없는 경우
-                if (num[i] == num[i + 1]) {
-                    dp[i + 1][j] = Math.max(dp[i + 1][j], dp[i][j] + 1);
-                }
-                else {
-                    // 다음 숫자와 동일한 숫자가 가지는 최대 유사도 + 1
-                    int tmp = 0;
-                    for (int k = i; k >= 0; k--) {
-                        if (num[k] != num[i + 1] || dp[k][j] == -1) continue;
-                        tmp = dp[k][j];
-                        break;
+        for(int i = 2; i <= n; i++) {
+            // i번째 숫자까지 고려해봤을 때
+            // 그 동안 숫자를 j번 변경했고
+            // 마지막으로 사용한 숫자가 k라 했을 때 (k는 1, 2, 3, 4중에 하나)
+            // 얻을 수 있는 최대 유사도를 계산합니다.
+            
+            // 이러한 상황을 만들기 위한 선택지는 크게 2가지 입니다.
+            for(int j = 0; j <= m; j++) {
+                for(int k = 1; k <= MAX_K; k++) {
+                    // i - 1번째에 사용한 숫자 l을 정합니다.
+                    for(int l = 1; l <= MAX_K; l++) {
+                        // Case 1
+                        // i번째 숫자에 k를 사용했지만, 숫자 변경이 일어나지 않은 경우입니다.
+                        // 따라서 숫자 변경이 없기 위해서는 l = k인 경우에만 유효한 경우이며, 
+                        // 숫자 변경 없이 지금까지의 총 변경 횟수가 j가 되려면
+                        // i - 1번째 까지의 총 변경 횟수 역시 j여야 하므로
+                        // dp[i - 1][j][l]에 
+                        // i번째 숫자에 k를 사용하므로 새롭게 얻어지는 유사도를 더한 값을
+                        // 얻을 수 있습니다.
+                        
+                        if(l == k)
+                            dp[i][j][k] = Math.max(dp[i][j][k], dp[i - 1][j][l] + (a[i] == k ? 1 : 0));
+                        
+                        // Case2
+                        // i번째 숫자에 k를 사용하여 숫자 변경이 일어난 경우입니다.
+                        // 따라서 숫자 변경이 일어나기 위해서는 l != k인 경우에만 유효하며, 
+                        // 숫자 변경이 발생해 지금까지의 총 변경 횟수가 j가 되려면
+                        // i - 1번째 까지의 총 변경 횟수는 j-1이어야 하므로
+                        // dp[i - 1][j - 1][l]에 
+                        // i번째 숫자에 k를 사용하므로 새롭게 얻어지는 유사도를 더한 값을
+                        // 얻을 수 있습니다.
+                        // 당연히 j > 0인 경우에만 만들어질 수 있는 경우입니다.
+                        if(l != k && j > 0)
+                            dp[i][j][k] = Math.max(dp[i][j][k], dp[i - 1][j - 1][l] + (a[i] == k ? 1 : 0));
                     }
-                    dp[i + 1][j] = Math.max(dp[i + 1][j], tmp + 1);
-
-                    // 숫자를 변경할 수 있는 횟수가 남아있는 경우
-                    if (j == m) continue;
-                    dp[i + 1][j + 1] = Math.max(dp[i + 1][j + 1], dp[i][j] + 1);
                 }
             }
         }
 
-        // 결과 계산
-        int ans = 0;
-        for (int i = 0; i < n; i++) {
-            // System.out.print(num[i] + " : ");
-            for (int j = 0; j <= m; j++) {
-                // System.out.print(dp[i][j] + " ");
-                ans = Math.max(ans, dp[i][j]);
-            }
-            // System.out.println();
-        }
+        // n개의 숫자에 대해 전부 고려했을 때,
+        // 숫자를 변경한 횟수가 m을 넘지 않으면서 
+        // 마지막으로 사용한 숫자가 k인 경우 중
+        // 가장 높은 유사도를 얻을 수 있는 경우를 선택합니다.
 
-        System.out.println(ans);
+        int ans = 0;
+
+        for(int j = 0; j <= m; j++)
+            for(int k = 1; k <= MAX_K; k++)
+                ans = Math.max(ans, dp[n][j][k]);
+
+        System.out.print(ans);
     }
 }
