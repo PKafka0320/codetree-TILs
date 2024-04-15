bb8437f186419ac38228759efdcafad43ab745ab
PKafka <66552990+PKafka0320@users.noreply.github.com>
2024-04-15 13:34:47 +0900

    Title: 최장 공통 부분 수열, Time: 229ms, Memory: 15MB, Status: Passed - CodeTree

--- 240415/최장 공통 부분 수열/longest-common-sequence.java
+++ 240415/최장 공통 부분 수열/longest-common-sequence.java
@@ -1,29 +1,55 @@
 import java.util.Scanner;
 
 public class Main {
-    static Scanner sc = new Scanner(System.in);
-    static String a, b;
-    static int[][] dp;
+    public static final int MAX_LEN = 1000;
+    public static final int UNUSED = -1;
+
+    public static String str1, str2;
+    public static int [][]memo = new int[MAX_LEN + 1][MAX_LEN + 1];
+
+    public static int str1Len, str2Len;
+
+    public static void initialize() {
+        for(int i = 1; i <= str1Len; i++)
+            for(int j = 1; j <= str2Len; j++)
+                memo[i][j] = UNUSED;
+    }
+
+    public static int findMaxLen(int i, int j) {
+        // 만약 주어진 문자열의 범위가 가능한 범위를 넘어가는 경우
+        // 더 이상 매칭을 진행할 수 없으므로, 
+        // 해당 상황에서 최장 증가 부분 수열의 길이는 0이 됩니다
+        if(i > str1Len || j > str2Len)
+            return 0;
+
+        // 이미 탐색한 적이 있다면 해당 값을 사용해줍니다.
+        if(memo[i][j] != UNUSED)
+            return memo[i][j];
+
+        // Case 1:
+        if(str1.charAt(i) == str2.charAt(j))
+            return memo[i][j] = findMaxLen(i + 1, j + 1) + 1;
+        // Case 2:
+        else
+            return memo[i][j] = Math.max(findMaxLen(i + 1, j), findMaxLen(i, j + 1));
+    }
 
     public static void main(String[] args) {
-        a = " " + sc.next();
-        b = " " + sc.next();
-        int n = a.length() - 1;
-        int m = b.length() - 1;
+        Scanner sc = new Scanner(System.in);
+        
+        str1 = sc.next();
+        str2 = sc.next();
 
-        // dp[i][j] : 첫 번째 문자열의 i번째, 두 번재 문자열의 j번째까지 확인했을때 최장 공통 부분 수열의 길이
-        dp = new int[n + 1][m + 1];
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
-                if (a.charAt(i) == b.charAt(j)) {
-                    dp[i][j] = dp[i - 1][j - 1] + 1;
-                }
-                else {
-                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
-                }
-            }
-        }
+        str1Len = str1.length();
+        str2Len = str2.length(); 
 
-        System.out.println(dp[n][m]);
+        // string의 index가 0부터 시작하기 때문에
+        // 이를 1부터 시작하기 위해서 앞에 #을 추가해줍니다.
+        str1 = "#" + str1;
+        str2 = "#" + str2;
+
+        initialize();
+
+        System.out.println(findMaxLen(1, 1));
     }
 }
