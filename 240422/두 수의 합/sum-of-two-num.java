df5443b3615ad0a86faed5733d10812f8fcbb817
PKafka <66552990+PKafka0320@users.noreply.github.com>
2024-04-22 11:06:04 +0900

    Title: 두 수의 합, Time: 398ms, Memory: 26MB, Status: Passed - CodeTree

--- 240422/두 수의 합/sum-of-two-num.java
+++ 240422/두 수의 합/sum-of-two-num.java
@@ -1,5 +1,8 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+import java.util.HashMap;
 
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
