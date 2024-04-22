40cbb4d88006b1e854df00e49ddf976874ac7770
PKafka <66552990+PKafka0320@users.noreply.github.com>
2024-04-22 10:17:21 +0900

    Title: 숫자 등장 횟수, Time: 784ms, Memory: 35MB, Status: Passed - CodeTree

--- 240422/숫자 등장 횟수/number-frequency.java
+++ 240422/숫자 등장 횟수/number-frequency.java
@@ -1,22 +1,29 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
 import java.util.HashMap;
 
 public class Main {
-    static Scanner sc = new Scanner(System.in);
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
     static HashMap<Integer, Integer> hm = new HashMap<>();
     static int n, m;
 
-    public static void main(String[] args) {
-        n = sc.nextInt();
-        m = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
 
+        st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            int num = sc.nextInt();
+            int num = Integer.parseInt(st.nextToken());
             hm.put(num, hm.getOrDefault(num, 0) + 1);
         }
 
+        st = new StringTokenizer(br.readLine());
         for (int i = 0; i < m; i++) {
-            int num = sc.nextInt();
+            int num = Integer.parseInt(st.nextToken());
             System.out.print(hm.getOrDefault(num, 0) + " ");
         }
     }
