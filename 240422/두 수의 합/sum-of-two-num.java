fe05d1bc9871337b9b7a0973dc80e6d22976e84e
PKafka <66552990+PKafka0320@users.noreply.github.com>
2024-04-22 10:52:50 +0900

    Title: 두 수의 합, Time: 2799ms, Memory: 176MB, Status: Memory Limit Exceed - CodeTree

---
+++ 240422/두 수의 합/sum-of-two-num.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static int n, k;
+    static int[] nums;
+    static HashMap<Integer, Integer> hm = new HashMap<>();
+
+    public static void main(String[] args) throws IOException {
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+
+        nums = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int sum = nums[i] + nums[j];
+                hm.put(sum, hm.getOrDefault(sum, 0) + 1);
+            }
+        }
+
+        System.out.println(hm.getOrDefault(k, 0));
+    }
+}
