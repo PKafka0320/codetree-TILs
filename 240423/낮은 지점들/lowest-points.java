7f1b17481718f805705f79ac38c5f6ce960e70a0
PKafka <66552990+PKafka0320@users.noreply.github.com>
2024-04-23 14:34:25 +0900

    Title: 낮은 지점들, Time: 590ms, Memory: 21MB, Status: Wrong Answer - CodeTree

---
+++ 240423/낮은 지점들/lowest-points.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static int n;
+    static HashMap<Integer, Integer> hm = new HashMap<>();
+
+    public static void main(String[] args) throws IOException {
+        n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            if (hm.containsKey(x)) {
+                hm.put(x, Math.min(y, hm.get(x)));
+            }
+            else {
+                hm.put(x, y);
+            }
+        }
+
+        int sum = 0;
+        for (int x : hm.keySet()) {
+            sum += hm.get(x);
+        }
+
+        System.out.println(sum);
+    }
+}
