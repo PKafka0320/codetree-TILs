d0e83650ed26885e230ede4f8b8b0b508fffd436
PKafka <66552990+PKafka0320@users.noreply.github.com>
2024-04-25 09:51:58 +0900

    Title: 정수 찾기, Time: 515ms, Memory: 30MB, Status: Passed - CodeTree

---
+++ 240425/정수 찾기/find-an-integer.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        
+        int n = Integer.parseInt(br.readLine());
+        HashSet<Integer> sh1 = new HashSet<>();
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            sh1.add(Integer.parseInt(st.nextToken()));
+        }
+        
+        int m = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < m; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            if (sh1.contains(num)) {
+                sb.append("1\n");
+            }
+            else {
+                sb.append("0\n");
+            }
+        }
+        System.out.println(sb);
+    }
+}
