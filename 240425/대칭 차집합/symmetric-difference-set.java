5dba7247fb4badf33f96889f2ee40ed3c641c93c
PKafka <66552990+PKafka0320@users.noreply.github.com>
2024-04-25 11:11:49 +0900

    Title: 대칭 차집합, Time: 1000ms, Memory: 51MB, Status: Passed - CodeTree

---
+++ 240425/대칭 차집합/symmetric-difference-set.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        HashSet<Integer> a = new HashSet<>();
+        HashSet<Integer> b = new HashSet<>();
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            a.add(Integer.parseInt(st.nextToken()));
+        }
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < m; i++) {
+            b.add(Integer.parseInt(st.nextToken()));
+        }
+
+        int ans = 0;
+        for (int num : a) {
+            if (b.contains(num)) continue;
+            ans++;
+        }
+        for (int num : b) {
+            if (a.contains(num)) continue;
+            ans++;
+        }
+
+        System.out.println(ans);
+    }
+}
