ee0021e3bbde57d5feee56cbe5b5e67236fe1f7d
PKafka <66552990+PKafka0320@users.noreply.github.com>
2024-04-25 10:08:39 +0900

    Title: 자리 바꾸기 2, Time: 99ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240425/자리 바꾸기 2/changing-seats-2.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        HashSet<Integer>[] hs = new HashSet[n + 1];
+
+        int[] num = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            hs[i] = new HashSet<>();
+            num[i] = i;
+            hs[i].add(i);
+        }
+
+        int k = Integer.parseInt(st.nextToken());
+        int[][] pos = new int[k][2];
+        for (int i = 0; i < k; i++) {
+            st = new StringTokenizer(br.readLine());
+            pos[i][0] = Integer.parseInt(st.nextToken());
+            pos[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < 3 * k; i++) {
+            int idx1 = pos[i % 4][0];
+            int idx2 = pos[i % 4][1];
+
+            hs[num[idx1]].add(idx2);
+            hs[num[idx2]].add(idx1);
+
+            int tmp = num[idx1];
+            num[idx1] = num[idx2];
+            num[idx2] = tmp;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            System.out.println(hs[i].size());
+        }
+    }
+}
