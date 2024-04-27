7001eb5f9349cae31bcf852143675b1f19f1bf3a
PKafka <66552990+PKafka0320@users.noreply.github.com>
2024-04-27 13:21:48 +0900

    Title: 자리 차지하기, Time: 431ms, Memory: 29MB, Status: Passed - CodeTree

--- 240427/자리 차지하기/take-place.java
+++ 240427/자리 차지하기/take-place.java
@@ -11,21 +11,21 @@ public class Main {
         int m = Integer.parseInt(st.nextToken());
 
         TreeSet<Integer> ts = new TreeSet<>();
+        for (int i = 1; i <= m; i++) {
+            ts.add(i);
+        }
+
         st = new StringTokenizer(br.readLine());
         int ans = 0;
         for (int i = 0 ; i < n; i++) {
             int num = Integer.parseInt(st.nextToken());
             
-            boolean isOver = true;
-            for (int j = num; j > 0; j--) {
-                if (ts.contains(j)) continue;
-                ts.add(j);
-                isOver = false;
+            Integer floor = ts.floor(num);
+            if (floor == null) break;
+            else {
                 ans++;
-                break;
+                ts.remove(floor);
             }
-            
-            if (isOver) break;
         }
 
         System.out.println(ans);
