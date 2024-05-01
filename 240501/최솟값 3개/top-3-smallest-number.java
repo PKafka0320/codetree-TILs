3962fe45921e3f9f370ea4590884b590ebdb3c91
PKafka <66552990+PKafka0320@users.noreply.github.com>
2024-05-01 18:31:18 +0900

    Title: 최솟값 3개, Time: 98ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240501/최솟값 3개/top-3-smallest-number.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+
+        int n = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        int res = 1;
+        for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(st.nextToken());
+
+            if (pq.size() < 2) {
+                pq.add(num);
+                res *= num;
+                sb.append("-1\n");
+                continue;
+            }
+            else if (pq.size() == 2) {
+                pq.add(num);
+                res *= num;
+                sb.append(res).append("\n");
+                continue;
+            }
+
+            int max = pq.peek();
+            if (max > num) {
+                res /= pq.poll();
+                pq.add(num);
+                res *= num;
+            }
+            sb.append(res).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
