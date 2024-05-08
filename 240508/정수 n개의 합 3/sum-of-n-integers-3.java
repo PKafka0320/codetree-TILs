bc79f5d8f13f72e492523e91ea3663462720becf
PKafka <66552990+PKafka0320@users.noreply.github.com>
2024-05-08 09:29:33 +0900

    Title: 정수 n개의 합 3, Time: 268ms, Memory: 21MB, Status: Passed - CodeTree

--- 240508/정수 n개의 합 3/sum-of-n-integers-3.java
+++ 240508/정수 n개의 합 3/sum-of-n-integers-3.java
@@ -9,8 +9,8 @@ public class Main {
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
 
-        int[][] grid = new int[n + 1][n + 1];
-        int[][] sum = new int[n + 1][n + 1];
+        int[][] grid = new int[n + 1][n + 1]; // 숫자 그리드
+        int[][] sum = new int[n + 1][n + 1]; //prefix sum 그리드
         for (int i = 0; i <= n; i++) {
             sum[i][0] = 0;
             sum[0][i] = 0;
@@ -21,12 +21,14 @@ public class Main {
             for (int j = 1; j <= n; j++) grid[i][j] = Integer.parseInt(st.nextToken());
         }
 
+        // 누적합 배열 계산
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n; j++) {
                 sum[i][j] = sum[i][j - 1] + sum[i - 1][j] + grid[i][j] - sum[i - 1][j - 1];
             }
         }
 
+        // 모든 구간에 대해 합을 찾아 최댓값 갱신
         int max = 0;
         for (int i = 0; i <= n - k; i++) {
             for (int j = 0; j <= n - k; j++) {
