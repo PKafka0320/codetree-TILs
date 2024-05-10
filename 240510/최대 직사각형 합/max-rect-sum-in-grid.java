ab11d1838733915753f7c0e6f68894c956da74e0
PKafka <66552990+PKafka0320@users.noreply.github.com>
2024-05-10 11:12:49 +0900

    Title: 최대 직사각형 합, Time: 488ms, Memory: 17MB, Status: Passed - CodeTree

--- 240510/최대 직사각형 합/max-rect-sum-in-grid.java
+++ 240510/최대 직사각형 합/max-rect-sum-in-grid.java
@@ -2,13 +2,18 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static int n;
+    static int[] dp;
+    static int[][] grid;
+    static int[][] prefixSum;
+
     public static void main(String[] args) throws Exception {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = Integer.parseInt(reader.readLine());
-
-        int[][] grid = new int[n + 1][n + 1];
-        int[][] prefixSum = new int[n + 1][n + 1];
+        n = Integer.parseInt(reader.readLine());
+        grid = new int[n + 1][n + 1];
+        prefixSum = new int[n + 1][n + 1];
+        dp = new int[n + 1];
 
         // 입력
         for (int row = 1; row <= n; row++) {
@@ -27,19 +32,37 @@ public class Main {
         }
 
         int max = Integer.MIN_VALUE;
-        // 사각형 범위에 따른 합의 최댓값
-        for (int r1 = 0; r1 < n; r1++) {
-            for (int c1 = 0; c1 < n; c1++) {
-                for (int r2 = r1 + 1; r2 <= n; r2++) {
-                    for (int c2 = c1 + 1; c2 <= n; c2++) {
-                        int tmp = prefixSum[r2][c2] - prefixSum[r2][c1] 
-                            - prefixSum[r1][c2] + prefixSum[r1][c1];
-                        max = Math.max(max, tmp);
-                    }
-                }
+        // 직사각형의 시작 행과 끝 행을 결정해서 각 쌍에 대해 가능한 직사각형 중 최대 합을 계산
+        for (int col1 = 1; col1 <= n; col1++) {
+            for (int col2 = col1; col2 <= n; col2++) {
+                max = Math.max(max, getMaxArea(col1, col2));
             }
         }
 
         System.out.println(max);
+    }
+
+    public static int getMaxArea(int x1, int x2) {
+        for(int y = 1; y <= n; y++) {
+            // 각 열의 합 계산
+            int value = getSum(x1, y, x2, y);
+    
+            // 이전까지의 합과 비교해서 최댓값 갱신
+            dp[y] = Math.max(value, dp[y - 1] + value);
+        }
+    
+        // dp 값 중 최댓값 확인
+        int maxArea = Integer.MIN_VALUE;
+        for(int y = 1; y <= n; y++) {
+            maxArea = Math.max(maxArea, dp[y]);
+        }
+
+        return maxArea;
+    }
+
+    // (x1, y1), (x2, y2) 직사각형 구간 내의 원소의 합 계산
+    public static int getSum(int x1, int y1, int x2, int y2) {
+        return prefixSum[x2][y2] - prefixSum[x1 - 1][y2] 
+            - prefixSum[x2][y1 - 1] + prefixSum[x1 - 1][y1 - 1];
     }
 }
