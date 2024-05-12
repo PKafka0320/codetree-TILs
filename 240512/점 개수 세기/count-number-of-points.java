4b1c177fd9021e1ba29bbe66b4fd12d813dc7c46
PKafka <66552990+PKafka0320@users.noreply.github.com>
2024-05-12 14:19:49 +0900

    Title: 점 개수 세기, Time: 82ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240512/점 개수 세기/count-number-of-points.java
+++ 240512/점 개수 세기/count-number-of-points.java
@@ -16,22 +16,26 @@ public class Main {
         numbers = new TreeSet<>(); // 숫자
         sequence = new HashMap<>(); // 순서
 
+        // 숫자 입력
         token = new StringTokenizer(reader.readLine());
         for (int idx = 0; idx < n; idx++) {
             numbers.add(Integer.parseInt(token.nextToken()));
         }
 
+        // 작은 수부터 순서대로 번호 부여
         int cnt = 0;
         for (int num : numbers) {
             sequence.put(num, cnt++);
         }
 
+        // 구간 내 점의 개수 계산
         StringBuilder answer = new StringBuilder();
         for (int query = 0; query < q; query++) {
             token = new StringTokenizer(reader.readLine());
 
-            int seqFrom = 0;
-            int seqTo = cnt - 1;
+            // 범위 내의 점이 없는 경우 방지
+            int seqFrom = cnt - 1;
+            int seqTo = 0;
 
             Integer numberFrom = numbers.ceiling(Integer.parseInt(token.nextToken()));
             if (numberFrom != null) seqFrom = sequence.get(numberFrom);
