d8a60a813d32ae8394cae8b10b199cca7b311167
PKafka <66552990+PKafka0320@users.noreply.github.com>
2024-05-15 21:22:28 +0900

    Title: 구간 외 최대 숫자, Time: 560ms, Memory: 38MB, Status: Passed - CodeTree

--- 240515/구간 외 최대 숫자/max-num-outside-of-interval.java
+++ 240515/구간 외 최대 숫자/max-num-outside-of-interval.java
@@ -1,20 +1,6 @@
 import java.io.*;
 import java.util.*;
 
-class Number implements Comparable<Number> {
-    int number, idx;
-
-    public Number(int number, int idx) {
-        this.number = number;
-        this.idx = idx;
-    }
-
-    @Override
-    public int compareTo(Number n) {
-        return this.number - n.number;
-    }
-}
-
 public class Main {
 
     public static void main(String[] args) throws Exception {
@@ -23,29 +9,38 @@ public class Main {
         StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
         int n = Integer.parseInt(tokenizer.nextToken()); // 숫자의 개수
         int q = Integer.parseInt(tokenizer.nextToken()); // 질의의 개수
-        Number[] numbers = new Number[n]; // 숫자 배열
+
+        int[] numbers = new int[n]; // 숫자 배열
+        int[] L = new int[n]; // L[i] : 0번부터 i번 원소 중 최댓값
+        int[] R = new int[n]; // R[i] : i번부터 n-1번 원소 중 최댓값
         
         // 숫자 입력
         tokenizer = new StringTokenizer(reader.readLine());
         for (int idx = 0; idx < n; idx++) {
-            numbers[idx] = new Number(Integer.parseInt(tokenizer.nextToken()), idx);
+            numbers[idx] = Integer.parseInt(tokenizer.nextToken());
+        }
+
+        // L 배열 생성
+        L[0] = numbers[0];
+        for(int idx = 1; idx < n; idx++) {
+            L[idx] = Math.max(L[idx - 1], numbers[idx]);
+        }
+
+        // L 배열 생성
+        R[n - 1] = numbers[n - 1];
+        for(int idx = n - 2; idx >= 0; idx--) {
+            R[idx] = Math.max(R[idx + 1], numbers[idx]);
         }
         
-        // 숫자를 크기 순으로 정렬
-        Arrays.sort(numbers);
-        
+        // 구간 [idxFrom, idxTo] 외 범위에서의 최댓값 계산
+        // L[idxFrom - 1], R[idxTo + 1] 중 더 큰 값
         StringBuilder answer = new StringBuilder();
         while (q-- > 0) {
             tokenizer = new StringTokenizer(reader.readLine());
             int idxFrom = Integer.parseInt(tokenizer.nextToken()) - 1;
             int idxTo = Integer.parseInt(tokenizer.nextToken()) - 1;
             
-            // 가장 큰 수부터 수의 번호가 범위에 속하는지 확인
-            for (int idx = n - 1; idx >= 0; idx--) {
-                if (idxFrom <= numbers[idx].idx && numbers[idx].idx <= idxTo) continue;
-                answer.append(numbers[idx].number).append("\n");
-                break;
-            }
+            answer.append(Math.max(L[idxFrom - 1], R[idxTo + 1])).append("\n");
         }
         System.out.println(answer);
     }
