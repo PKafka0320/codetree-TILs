68e8e2b3d8a857cabc1b8b09325165154e32b0a1
PKafka <66552990+PKafka0320@users.noreply.github.com>
2024-05-18 12:23:12 +0900

    Title: 컴퓨터 이용시간, Time: 1020ms, Memory: 33MB, Status: Passed - CodeTree

--- 240518/컴퓨터 이용시간/computer-hours.java
+++ 240518/컴퓨터 이용시간/computer-hours.java
@@ -1,6 +1,21 @@
 import java.io.*;
 import java.util.*;
 
+class Tuple implements Comparable<Tuple> { 
+    int time, value, index;
+    public Tuple(int time, int value, int index) { 
+        this.time = time; 
+        this.value = value;
+        this.index = index; 
+    }
+    @Override
+    public int compareTo(Tuple t) {
+        if(time != t.time) return time - t.time;
+        if(value != t.value) return value - t.value;
+        return index - t.index;
+    }
+}
+
 public class Main {
 
     public static void main(String[] args) throws Exception {
@@ -8,36 +23,38 @@ public class Main {
 
         int n = Integer.parseInt(reader.readLine()); // 사람의 수
         
-        int[] startTime = new int[n + 1]; // [i] : i번째 사람이 컴퓨터를 시작하는 시간
-        TreeMap<Integer, Integer> times = new TreeMap<>(); // 컴퓨터 시작 시간과 종료 시간
-        TreeMap<Integer, Integer> pcNumbers = new TreeMap<>(); // 시작 시간에 사용되는 컴퓨터 번호
-        TreeSet<Integer> enablePc = new TreeSet<>(); // 사용 가능한 컴퓨터 번호
-        HashMap<Integer, Integer> endStartTime = new HashMap<>(); // 종료 시간과 연관된 시작 시간
+        int[] assignedNums = new int[n + 1]; // [i] : i번째 사람에게 할당된 컴퓨터 번호
+        PriorityQueue<Integer> computers = new PriorityQueue<>(); // 할당 가능한 컴퓨터 번호
+        ArrayList<Tuple> points = new ArrayList<>(); // 사람이 컴퓨터를 사용하는 시작 시간과 끝 시간
         
         for (int idx = 1; idx <= n; idx++) {
             StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
             int start = Integer.parseInt(tokenizer.nextToken());
             int end = Integer.parseInt(tokenizer.nextToken());
             
-            startTime[idx] = start;
-            times.put(start, 1);
-            times.put(end, -1);
-            enablePc.add(idx);
-            endStartTime.put(end, start);
+            computers.add(idx);
+            points.add(new Tuple(start, 1, idx)); // 시작 시간
+            points.add(new Tuple(end, -1, idx)); // 끝 시간
         }
         
-        for (int time : times.keySet()) {
-            if (times.get(time) > 0) {
-                pcNumbers.put(time, enablePc.pollFirst());
+        Collections.sort(points);
+        
+        for (Tuple point : points) {
+            int time = point.time;
+            int value = point.value;
+            int idx = point.index;
+
+            if (value == 1) {
+                assignedNums[idx] = computers.poll();
             }
             else {
-                enablePc.add(pcNumbers.get(endStartTime.get(time)));
+                computers.add(assignedNums[idx]);
             }
         }
         
         StringBuilder answer = new StringBuilder();
         for (int idx = 1; idx <= n; idx++) {
-            answer.append(pcNumbers.get(startTime[idx])).append(" ");
+            answer.append(assignedNums[idx]).append(" ");
         }
         System.out.println(answer);
     }
