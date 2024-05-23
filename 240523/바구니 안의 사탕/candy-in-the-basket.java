2bc7e86c8903244d7d9689e996dab545b776e1fe
PKafka <66552990+PKafka0320@users.noreply.github.com>
2024-05-23 11:34:50 +0900

    Title: 바구니 안의 사탕, Time: 490ms, Memory: 24MB, Status: Passed - CodeTree

--- 240523/바구니 안의 사탕/candy-in-the-basket.java
+++ 240523/바구니 안의 사탕/candy-in-the-basket.java
@@ -1,6 +1,20 @@
 import java.io.*;
 import java.util.*;
 
+class Candy implements Comparable<Candy> {
+    int position, count;
+
+    public Candy(int position, int count) {
+        this.position = position;
+        this.count = count;
+    }
+
+    @Override
+    public int compareTo(Candy c) {
+        return this.position - c.position; // position 기준 오름차순 정렬
+    }
+}
+
 public class Main {
 
     public static void main(String[] args) throws Exception {
@@ -10,7 +24,8 @@ public class Main {
         int N = Integer.parseInt(tokenizer.nextToken()); // 바구니의 개수
         int K = Integer.parseInt(tokenizer.nextToken()) * 2; // 구간의 길이
         
-        HashMap<Integer, Long> candies = new HashMap<>(); // <K, V>: K위치의 사탕의 개수
+        Candy[] candies = new Candy[N]; // [i]: i번째 바구니의 정보
+        // HashMap<Integer, Long> candies = new HashMap<>(); // <K, V>: K위치의 사탕의 개수
         int maxPosition = 0; // 최대 위치
         int minPosition = 1_000_000; // 최소 위치
         
@@ -20,26 +35,28 @@ public class Main {
             int count = Integer.parseInt(tokenizer.nextToken()); // 사탕의 개수
             int position = Integer.parseInt(tokenizer.nextToken()); // 바구니의 좌표
             
-            candies.put(position, candies.getOrDefault(position, 0L) + count);
-            maxPosition = Math.max(maxPosition, position);
-            minPosition = Math.min(minPosition, position);
+            candies[idx] = new Candy(position, count);
+            // candies.put(position, candies.getOrDefault(position, 0L) + count);
+            // maxPosition = Math.max(maxPosition, position);
+            // minPosition = Math.min(minPosition, position);
         }
+
+        Arrays.sort(candies);
         
-        int endPosition = 0; // 구간의 끝 위치
+        int endIdx = 0; // 구간의 끝 위치
         long sum = 0; // 구간 내 사탕의 합
         long max = 0; // 사탕의 합의 최댓값
         
-        // 구간의 시작 지점부터 구간의 길이 내의 사탕의 합 계산
-        for (int startPosition = minPosition; startPosition <= maxPosition; startPosition++) {
-            while (endPosition <= maxPosition && endPosition - startPosition <= K) {
-                sum += candies.getOrDefault(endPosition, 0L);
-                endPosition++;
+        // 구간 내의 사탕의 합 계산
+        for (int startIdx = 0; startIdx < N; startIdx++) {
+            while (endIdx < N && candies[endIdx].position - candies[startIdx].position <= K) {
+                sum += candies[endIdx].count;
+                endIdx++;
             }
             
             max = Math.max(max, sum);
-            if (endPosition > maxPosition) break;
             
-            sum -= candies.getOrDefault(startPosition, 0L);
+            sum -= candies[startIdx].count;
         }
         
         System.out.println(max);
