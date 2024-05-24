62b8d42b22e80f42ff6ab7e4415b17b40df6abbf
PKafka <66552990+PKafka0320@users.noreply.github.com>
2024-05-24 11:46:58 +0900

    Title: 최소가 되는 좌표 범위의 차, Time: 92ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240524/최소가 되는 좌표 범위의 차/the-minimum-difference-in-coordinate-range.java
+++ 240524/최소가 되는 좌표 범위의 차/the-minimum-difference-in-coordinate-range.java
@@ -1,6 +1,8 @@
 import java.io.*;
 import java.util.*;
 
+import com.sun.org.apache.bcel.internal.generic.NEW;
+
 class Pair implements Comparable<Pair> {
     int x, y;
     
@@ -11,11 +13,33 @@ class Pair implements Comparable<Pair> {
 
     @Override
     public int compareTo(Pair p) {
-        return this.y - p.y;
+        return this.x - p.x; // x 기준 오름차순 정렬
+    }
+    
+    @Override
+    public String toString() {
+        return "[" + x + "," + y + "]";
+    }
+}
+
+class TargetPoint implements Comparable<TargetPoint> {
+    int x, y;
+
+    public TargetPoint(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public int compareTo(TargetPoint p) {
+        if(this.y != p.y)
+            return this.y - p.y; // y 기준 오름차순 정렬
+        return this.x - p.x; // x 기준 오름차순 정렬
     }
 }
 
 public class Main {
+    static TreeSet<TargetPoint> pointCount = new TreeSet<>(); // 두 점과 그 사이의 점들
 
     public static void main(String[] args) throws Exception {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
@@ -24,7 +48,7 @@ public class Main {
         int N = Integer.parseInt(tokenizer.nextToken()); // 점의 개수
         int D = Integer.parseInt(tokenizer.nextToken()); // 두 점의 y 좌표의 최소 차이
 
-        Pair[] pairs = new Pair[N];
+        Pair[] pairs = new Pair[N]; // [i]: i번째 점
         
         for (int idx = 0; idx < N; idx++) {
             tokenizer = new StringTokenizer(reader.readLine());
@@ -36,18 +60,29 @@ public class Main {
         
         Arrays.sort(pairs);
         
-        int secondIdx = N - 1; // 두 번째 점의 인덱스
+        System.out.println(Arrays.toString(pairs));
+        
+        int secondIdx = -1; // 두 번째 점의 인덱스
         int min = Integer.MAX_VALUE; // 두 점의 x 좌표의 최소 차이
         
+        // 구간 생성
         for (int firstIdx = 0; firstIdx < N; firstIdx++) {
-            if (firstIdx < secondIdx && Math.abs(pairs[firstIdx].y - pairs[secondIdx].y) >= D) {
-                min = Math.min(min, Math.abs(pairs[firstIdx].x - pairs[secondIdx].x));
+            // y좌표 차가 d가 되기 전까지 진행
+            while (secondIdx + 1 < N && getMax() - getMin() < D) {
+                pointCount.add(new TargetPoint(pairs[secondIdx + 1].x, pairs[secondIdx + 1].y));
+                secondIdx++;
             }
             
-            while (secondIdx >= 0 && Math.abs(pairs[firstIdx].y - pairs[secondIdx].y) >= D) {
-                min = Math.min(min, Math.abs(pairs[firstIdx].x - pairs[secondIdx].x));
-                secondIdx--;
+            // 최대한 이동했는데도 조건에 만족하지 못하면 종료
+            if (getMax() - getMin() < D) {
+                break;
             }
+            
+            // 구간 크기 중 최솟값 갱신
+            min = Math.min(min, pairs[secondIdx].x - pairs[firstIdx].x);
+            
+            // 첫 번째 점의 정보 제거
+            pointCount.remove(new TargetPoint(pairs[firstIdx].x, pairs[firstIdx].y));
         }
         
         if (min == Integer.MAX_VALUE) {
@@ -55,5 +90,15 @@ public class Main {
         }
         
         System.out.println(min);
+    }
+    
+    public static int getMin() {
+        if(pointCount.isEmpty()) return 0;
+        return pointCount.first().y; 
+    }
+    
+    public static int getMax() {
+        if(pointCount.isEmpty()) return 0;
+        return pointCount.last().y;
     }
 }
