29bd04ac47748447d60dfd11343b31f27dcfdbc8
PKafka <66552990+PKafka0320@users.noreply.github.com>
2024-05-26 10:27:35 +0900

    Title: 양쪽 모두 존재하는 구간, Time: 281ms, Memory: 23MB, Status: Passed - CodeTree

--- 240526/양쪽 모두 존재하는 구간/both-exist-section.java
+++ 240526/양쪽 모두 존재하는 구간/both-exist-section.java
@@ -2,6 +2,11 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static int[] numbers;
+    static int[] countArrayIn;
+    static int[] countArrayOut;
+    static int distinctNumIn;  // 구간 내 서로 다른 숫자 개수
+    static int distinctNumOut; // 구간 외 서로 다른 숫자 개수
 
     public static void main(String[] args) throws Exception {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
@@ -10,56 +15,47 @@ public class Main {
         int n  = Integer.parseInt(tokenizer.nextToken()); // 숫자의 개수
         int m  = Integer.parseInt(tokenizer.nextToken()); // 구간 안, 밖에 있어야 하는 1이상 m이하의 숫자
         
-        HashMap<Integer, Integer> inCounts = new HashMap<>(); // <K, V>: 수열 안에 있는 숫자 K의 개수 V
-        HashMap<Integer, Integer> outCounts = new HashMap<>(); // <K, V>: 수열 밖에 있는 숫자 K의 개수 V
-        int[] numbers = new int[n]; // [i]: i번째 숫자
+        numbers = new int[n]; // [i]: i번째 숫자
+        countArrayIn = new int[m + 1];  // 구간 내 숫자 정보
+        countArrayOut = new int[m + 1]; // 구간 외 숫자 정보
         
         // 숫자 배열 입력
         tokenizer = new StringTokenizer(reader.readLine());
         for (int idx = 0; idx < n; idx++) {
             numbers[idx] = Integer.parseInt(tokenizer.nextToken());
             
-            if (numbers[idx] <= m) {
-                outCounts.put(numbers[idx], outCounts.getOrDefault(numbers[idx], 0) + 1);
-            }
+            // 해당 숫자가 최초로 등장했다면 구간 외 숫자의 종류 증가
+            if(countArrayOut[numbers[idx]] == 0)
+                distinctNumOut++;
+            
+            countArrayOut[numbers[idx]]++;
         }
         
         int endIdx = 0; // 구간의 끝 위치
         int min = Integer.MAX_VALUE; // 구간의 최소 길이
+        
+        // 구간 결정 및 계산
         for (int startIdx = 0; startIdx < n; startIdx++) {
             // m 이하의 숫자가 아니라면 무시
             if (numbers[startIdx] > m) continue;
             
             // 선택한 구간 내에 1부터 m까지의 숫자가 모두 있을때까지 구간의 끝 이동
-            while (endIdx < n && inCounts.size() < m) {
-                // m 이하의 숫자가 아니라면 무시
-                if (numbers[endIdx] <= m) {
-                    // 구간 안에 해당 숫자 추가, 구간 밖에 해당 숫자 제거
-                    inCounts.put(numbers[endIdx], inCounts.getOrDefault(numbers[endIdx], 0) + 1);
-                    outCounts.put(numbers[endIdx], outCounts.get(numbers[endIdx]) - 1);
-                    
-                    // 숫자의 개수가 0개일 경우 목록에서 제거
-                    if (outCounts.getOrDefault(numbers[endIdx], 0) == 0) {
-                        outCounts.remove(numbers[endIdx]);
-                    }
-                }
+            while (endIdx < n && distinctNumIn < m) {
+                push(endIdx);
                 endIdx++;
             }
             
-            // 구간의 안, 밖에 1이상 m이하의 숫자가 있는 경우에만 최소 길이 갱신
-            if (inCounts.size() == m && outCounts.size() == m) {
+            // 가능한 경우가 없는 경우 종료
+            if (distinctNumIn < m) break;
+            
+            // 구간의 안에도 1이상 m이하의 숫자가 있는 경우에만 최소 길이 갱신
+            if (distinctNumOut == m) {
                 min = Math.min(min, endIdx - startIdx);
             }
 
             // 구간의 시작 위치를 이동하기 전,
-            // 구간 안에 해당 숫자 제거, 구간 밖에 해당 숫자 추가
-            outCounts.put(numbers[startIdx], outCounts.getOrDefault(numbers[startIdx], 0) + 1);
-            inCounts.put(numbers[startIdx], inCounts.get(numbers[startIdx]) - 1);
-            
-            // 숫자의 개수가 0개일 경우 목록에서 제거
-            if (inCounts.getOrDefault(numbers[startIdx], 0) == 0) {
-                inCounts.remove(numbers[startIdx]);
-            }
+            // 해당 숫자를 구간에서 제거
+            pop(startIdx);
         }
         
         // 만족하는 경우가 없는 경우 정답 수정
@@ -68,5 +64,35 @@ public class Main {
         }
         
         System.out.println(min);
+    }
+    
+    // idx번째 숫자를 구간에 집어넣게 되었을 때의 countArray와 distinctNum 값을 갱신
+    public static void push(int idx) {
+        int num = numbers[idx];
+    
+        // 구간 내로 새로 들어온 숫자라면 구간 내 숫자의 종류 증가
+        if(countArrayIn[num] == 0)
+            distinctNumIn++;
+        countArrayIn[num]++;
+    
+        // num과 동일한 구간 외 숫자가 없다면 구간 외 숫자의 종류 감소
+        countArrayOut[num]--;
+        if(countArrayOut[num] == 0)
+            distinctNumOut--;
+    }
+    
+    // idx번째 숫자를 구간에서 빼게 되었을 때의 countArray와 distinctNum 값을 갱신
+    public static void pop(int idx) {
+        int num = numbers[idx];
+    
+        // num과 동일한 구간 내 숫자가 없다면 구간 내 숫자의 종류 감소
+        countArrayIn[num]--;
+        if(countArrayIn[num] == 0)
+            distinctNumIn--;
+    
+        // 구간 외로 새로 들어온 숫자라면 구간 외 숫자의 종류 증가
+        if(countArrayOut[num] == 0)
+            distinctNumOut++; 
+        countArrayOut[num]++;
     }
 }
