ae771b21816602259e24182a9eb9a806092cda74
PKafka <66552990+PKafka0320@users.noreply.github.com>
2024-05-28 12:09:25 +0900

    Title: 부분수열 가능성 판단하기, Time: 247ms, Memory: 21MB, Status: Passed - CodeTree

--- 240528/부분수열 가능성 판단하기/determine-possibility-of-subsequence.java
+++ 240528/부분수열 가능성 판단하기/determine-possibility-of-subsequence.java
@@ -10,43 +10,49 @@ public class Main {
         int n = Integer.parseInt(tokenizer.nextToken()); // 수열 A의 원소의 개수
         int m = Integer.parseInt(tokenizer.nextToken()); // 수열 B의 원소의 개수
         
-        int[] A = new int[n]; // 수열 A
-        int[] B = new int[m]; // 수열 B
+        int[] A = new int[n + 1]; // 수열 A
+        int[] B = new int[m + 1]; // 수열 B
+        int[] L = new int[m + 2]; // [i]: 수열 B의 1번부터 i번까지 1 -> i 순서대로 수열 A의 왼쪽부터 순서대로 매칭헀을 때 마지막으로 매칭되는 수열 A 원소의 위치
+        int[] R = new int[m + 2]; // [i]: 수열 B의 m번부터 i번까지 n -> i 순서대로 수열 A의 오른쪽부터 순서대로 매칭헀을 때 마지막으로 매칭되는 수열 A 원소의 위치
 
         // 수열 A 입력
         tokenizer = new StringTokenizer(reader.readLine());
-        for (int idx = 0; idx < n; idx++) {
+        for (int idx = 1; idx <= n; idx++) {
             A[idx] = Integer.parseInt(tokenizer.nextToken());
         }
 
         // 수열 B 입력
         tokenizer = new StringTokenizer(reader.readLine());
-        for (int idx = 0; idx < m; idx++) {
+        for (int idx = 1; idx <= m; idx++) {
             B[idx] = Integer.parseInt(tokenizer.nextToken());
         }
         
-        int count = 0; // 부분 수열의 가짓수
-        
-        // 제거할 원소를 선택
-        for (int removeIdx = 0; removeIdx < m; removeIdx++) {
-            int idxA = 0; // 수열 A에서 현재 확인하고 있는 원소 위치
-            int idxB = 0; // 수열 B에서 현재 확인하고 있는 원소 위치
-            
-            for (idxB = 0; idxB < m; idxB++) {
-                if (idxB == removeIdx) continue;
-                
-                // B에서 현재 확인하고 있는 원소와 동일한 원소가 나올때까지
-                // A에서 확인하고 있는 포인터 이동
-                while (idxA < n && A[idxA] != B[idxB]) {
-                    idxA++;
-                }
-                
-                // 끝까지 확인한 경우 종료
-                if (idxA == n) break;
+        // L 배열 생성
+        for (int idxA = 1, idxB = 1; idxB <= m; idxB++) {
+            while (idxA <= n && A[idxA] != B[idxB]) {
+                idxA++;
             }
             
-            if (idxA == n && idxB != m) continue;
-            count++;
+            L[idxB] = idxA;
+        }
+        
+        // R 배열 생성
+        for (int idxA = n, idxB = m; idxB >= 1; idxB--) {
+            while (idxA >= 1 && A[idxA] != B[idxB]) {
+                idxA--;
+            }
+            
+            R[idxB] = idxA;
+        }
+        
+        // 경계 부분 처리
+        L[0] = 0;
+        R[m + 1] = n + 1;
+        
+        int count = 0; // 부분 수열의 가짓수
+        
+        for (int idxB = 1; idxB <= m; idxB++) {
+            if (L[idxB - 1] < R[idxB + 1]) count++;
         }
         
         System.out.println(count);
