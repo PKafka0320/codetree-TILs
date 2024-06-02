455ad2755d1e8079a9ba04fd1a85fec96066a577
PKafka <66552990+PKafka0320@users.noreply.github.com>
2024-06-02 11:32:16 +0900

    Title: 자연수 n개의 합, Time: 86ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240602/자연수 n개의 합/sum-of-n-natural-numbers.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        long target = Long.parseLong(reader.readLine()); // 자연수 합의 최댓값
+        
+        long left = 1; // 가능한 n의 구간 시작
+        long right = target; // 가능한 n의 구간 끝
+        long answer = 0; // n의 최댓값
+        
+        while (left <= right) {
+            long mid = (left + right) / 2; // 중앙값
+            
+            if (mid * (mid + 1) / 2 <= target) {
+                answer = Math.max(answer, mid);
+                left = mid + 1;
+            }
+            else {
+                right = mid - 1;
+            }
+        }
+        
+        System.out.println(answer);
+    }
+}
