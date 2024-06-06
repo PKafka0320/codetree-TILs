2eadab34f5753082b5a2eb03c6572c3ffba358a6
PKafka <66552990+PKafka0320@users.noreply.github.com>
2024-06-06 12:44:49 +0900

    Title: 최소 통과 시간, Time: 249ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240606/최소 통과 시간/minimum-transit-time.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
+        int n = Integer.parseInt(tokenizer.nextToken()); // 물건의 개수
+        int m = Integer.parseInt(tokenizer.nextToken()); // 통로의 개수
+        
+        int[] paths = new int[m]; // [i]: i번째 통로를 통과하는데 걸리는 시간
+        
+        // 통로 입력
+        for (int idx = 0; idx < m; idx++) {
+            paths[idx] = Integer.parseInt(reader.readLine());
+        }
+        
+        Arrays.sort(paths);
+        
+        int low = 1; // 최소 시간 범위의 시작
+        int high = paths[m - 1] * n; // 최소 시간 범위의 끝
+        int min = high; // 최소 시간
+        
+        // Parametric Search
+        while (low <= high) {
+            int mid = (low + high) / 2; // 최소 시간 범위의 중앙값
+            int count = 0; // 중앙값에 통과되는 물건의 개수
+
+            for (int idx = 0; idx < m; idx++) {
+                count += mid / paths[idx];
+                
+                if (count >= n) break;
+            }
+            
+            if (count >= n) {
+                high = mid - 1;
+                min = Math.min(min, mid);
+            }
+            else {
+                low = mid + 1;
+            }
+        }
+        
+        System.out.println(min);
+    }
+}
