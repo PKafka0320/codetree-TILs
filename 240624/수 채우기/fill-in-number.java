1a9efbc92fbb923768c1f445a54fdcb8f636f48e
PKafka <66552990+PKafka0320@users.noreply.github.com>
2024-06-24 21:16:20 +0900

    Title: 수 채우기, Time: 104ms, Memory: 8MB, Status: Passed - CodeTree

--- 240624/수 채우기/fill-in-number.java
+++ 240624/수 채우기/fill-in-number.java
@@ -7,18 +7,18 @@ public class Main {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(reader.readLine()); // 만들고자 하는 금액
+        int max = n / 5; // 5원 동전의 최대 개수
         
-        int answer = 0;
-        answer += n / 5;
-        n %= 5;
-        answer += n / 2;
-        n %= 2;
+        while (max >= 0) {
+            int left = n - (max * 5);
+            
+            if (left % 2 == 0) {
+                System.out.println(max + (left / 2));
+                return;
+            }
+            max--;
+        }
         
-        if (n == 0) {
-            System.out.println(answer);
-        }
-        else {
-            System.out.println(-1);
-        }
+        System.out.println(-1);
     }
 }
