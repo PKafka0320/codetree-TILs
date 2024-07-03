6c76dea83342592bf5d46b9711c6c5c136a7ab3d
PKafka <66552990+PKafka0320@users.noreply.github.com>
2024-07-03 21:28:52 +0900

    Title: 폭탄 해체 작업, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240703/폭탄 해체 작업/the-bomb-dismantling.java
+++ 240703/폭탄 해체 작업/the-bomb-dismantling.java
@@ -1,57 +1,58 @@
 import java.io.*;
 import java.util.*;
 
-class Bomb implements Comparable<Bomb> {
-	int value, time;
-	
-	public Bomb(int value, int time) {
-		this.value = value;
-		this.time = time;
-	}
-	
-	@Override
-	public int compareTo(Bomb other) {
-		if (this.time == other.time) return other.value - this.value;
-		return this.time - other.time;
-	}
+class Bomb implements Comparable<Bomb> { 
+    int timeLimit, score;
+    
+    public Bomb(int timeLimit, int score) { 
+        this.timeLimit = timeLimit; 
+        this.score = score; 
+    }
+    
+    @Override
+    public int compareTo(Bomb other) {
+        if(timeLimit != other.timeLimit) return timeLimit - other.timeLimit;
+        return score - other.score;
+    }
 }
 
 public class Main {
-	public static void main(String[] args) throws Exception {
-		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer tokenizer;
-		
-		int n = Integer.parseInt(reader.readLine()); // 폭탄의 개수
-		Bomb[] bombs = new Bomb[n]; // [i]: i번째 폭탄의 정보
-		
-		for (int idx = 0; idx < n; idx++) {
-			tokenizer = new StringTokenizer(reader.readLine());
-			int value = Integer.parseInt(tokenizer.nextToken());
-			int time = Integer.parseInt(tokenizer.nextToken());
-			
-			bombs[idx] = new Bomb(value, time);
-		}
-		
-		Arrays.sort(bombs);
-		
-		int time = 0;
-		int idx = 0;
-		int sum = 0;
-		while (idx < n) {
-            // 폭탄이 터지기 전인지 확인
-			if (bombs[idx].time > time) {
-				sum += bombs[idx].value;
-				time++;
-				idx++;
-			}
-            // 터진 폭탄들 스킵하기
-			else {
-				while (bombs[idx].time <= time) {
-					idx++;
-				}
-			}
-		}
-		
-		System.out.println(sum);
-	}
+
+    public static void main(String[] args) throws Exception {
+        System.setIn(new FileInputStream("src/input.txt"));
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer tokenizer;
+
+        int n = Integer.parseInt(reader.readLine()); // 폭탄의 수
+        int maxTime = 0; // 폭탄의 시간 중 최댓값
+        Bomb[] bombs = new Bomb[n]; // [i]: i번째 폭탄
+        PriorityQueue<Integer> pq = new PriorityQueue<>(); // 시간에 따른 폭탄의 우선순위
+        
+        for (int idx = 0; idx < n; idx++) {
+            tokenizer = new StringTokenizer(reader.readLine());
+            int score = Integer.parseInt(tokenizer.nextToken());
+            int timeLimit = Integer.parseInt(tokenizer.nextToken());
+            
+            maxTime = Math.max(maxTime, timeLimit);            
+            bombs[idx] = new Bomb(timeLimit, score);
+        }
+        
+        Arrays.sort(bombs);
+        
+        int bombIdx = n - 1;
+        int answer = 0;
+        
+        for (int time = maxTime; time >= 1; time--) {
+            while (bombIdx >= 0 && bombs[bombIdx].timeLimit >= time) {
+                pq.add(-bombs[bombIdx].score);
+                bombIdx--;
+            }
+            
+            if (!pq.isEmpty()) {
+                answer += -pq.poll();
+            }
+        }
+        
+        System.out.println(answer);
+    }
 }
