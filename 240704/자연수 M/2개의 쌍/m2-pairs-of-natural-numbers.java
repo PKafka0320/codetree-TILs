93cc2cfb9b82524d3a6241724acc916d2a4f3b8a
PKafka <66552990+PKafka0320@users.noreply.github.com>
2024-07-04 20:17:46 +0900

    Title: 자연수 M/2개의 쌍, Time: 1273ms, Memory: 54MB, Status: Passed - CodeTree

--- 240704/자연수 M/2개의 쌍/m2-pairs-of-natural-numbers.java
+++ 240704/자연수 M/2개의 쌍/m2-pairs-of-natural-numbers.java
@@ -1,34 +1,77 @@
-import java.io.*;
-import java.util.*;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Collections;
 
-public class Main {
-    public static void main(String[] args) throws Exception{
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer tokenizer;
+class Pair implements Comparable<Pair> {
+    int y, x;
+    Pair(int y, int x) {
+        this.y = y;
+        this.x = x;
+    }
 
-        int n = Integer.parseInt(reader.readLine()); // 서로 다른 자연수의 개수
-        PriorityQueue<Integer> pq = new PriorityQueue<>(); // 모든 자연수 목록
+    @Override
+    public int compareTo(Pair p) {
+        return this.y - p.y;        // y 기준 오름차순으로 정렬합니다.
+    }
+}
 
-        for (int idx = 0; idx < n; idx++) {
-            tokenizer = new StringTokenizer(reader.readLine());
-            int count = Integer.parseInt(tokenizer.nextToken());
-            int number = Integer.parseInt(tokenizer.nextToken());
+public class Main {    
+    // 변수 선언
+    public static int n;
+    public static ArrayList<Pair> nums = new ArrayList<>();
+    
+    public static int ans;
 
-            for (int currentCount = 0; currentCount < count; currentCount++) {
-                pq.add(number);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // 입력:
+        n = sc.nextInt();
+        for(int i = 0; i < n; i++) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            nums.add(new Pair(y, x));
+        }
+
+        // y 기준으로 오름차순 정렬을 진행합니다.
+        Collections.sort(nums);
+
+        // 최선의 전략을 오름차순 정렬 뒤
+        // 가장 작은 수와 큰 수를 하나씩 매칭해주는 것입니다.
+        int li = 0, ri = n - 1;
+        while(li <= ri) {
+            int ly = nums.get(li).y;
+            int lx = nums.get(li).x;
+            int ry = nums.get(ri).y;
+            int rx = nums.get(ri).x;
+
+            // 답을 갱신합니다.
+            ans = Math.max(ans, ly + ry);
+
+            // 왼쪽 개수가 더 적다면
+            // 왼쪽을 전부 매칭시켜줍니다.
+            if(lx < rx) {
+                // 오른쪽은 lx만큼 개수를 줄여줍니다.
+                nums.set(ri, new Pair(ry, rx - lx));
+
+                // 왼쪽 위치를 한칸 뒤로 옮겨줍니다.
+                li++;
+            }
+            // 오른쪽 개수가 더 적다면
+            // 오른쪽을 전부 매칭시켜줍니다.
+            else if(lx > rx) {
+                // 왼쪽은 rx만큼 개수를 줄여줍니다.
+                nums.set(li, new Pair(ly, lx - rx));
+
+                // 오른쪽 위치를 한칸 앞으로 옮겨줍니다.
+                ri--;
+            }
+            // 개수가 동일하다면
+            // li, ri 위치 모두 옮겨줍니다.
+            else {
+                li++;
+                ri--;
             }
         }
-
-        Object[] sums = pq.toArray();
-        
-        int min = (int) sums[0] + (int) sums[sums.length - 1];
-        int startIdx = 1;
-        int endIdx = sums.length - 2;
-
-        while (startIdx < endIdx) {
-            min = Math.min(min,(int) sums[startIdx++] + (int) sums[endIdx--]);
-        }
-
-        System.out.println(min);
+        System.out.print(ans);
     }
 }
