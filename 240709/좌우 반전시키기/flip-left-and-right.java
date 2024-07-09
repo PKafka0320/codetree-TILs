e58f4cecbeb5c9401333363c8dcdf65c32f21ffa
PKafka <66552990+PKafka0320@users.noreply.github.com>
2024-07-09 11:53:14 +0900

    Title: 좌우 반전시키기, Time: 105ms, Memory: 8MB, Status: Passed - CodeTree

--- 240709/좌우 반전시키기/flip-left-and-right.java
+++ 240709/좌우 반전시키기/flip-left-and-right.java
@@ -2,34 +2,32 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        int n = Integer.parseInt(br.readLine()); // 숫자의 개수
-        int[] numbers = new int[n]; // [i]: i번째 숫자
-        int count = 0; // 반전 횟수
-
+        int n = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int idx = 0; idx < n; idx++) {
-            numbers[idx] = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
-
-        // idx 위치의 숫자가 0일 경우 idx + 1의 숫자와 그 양 옆을 뒤집기
-        for (int idx = 0; idx < n - 1; idx++) {
-            if (numbers[idx] == 0) {
-                count++;
-                numbers[idx] = 1 - numbers[idx];
-                numbers[idx + 1] = 1 - numbers[idx + 1];
-                if (idx == n - 2) continue; // 마지막 숫자는 왼쪽만 뒤집기
-                numbers[idx + 2] = 1 - numbers[idx + 2];
+        int cnt = 0;
+        for(int i=1;i<n-1;i++) {
+            if(arr[i-1]==0) {
+                arr[i-1] = 1- arr[i-1];
+                arr[i] = 1- arr[i];
+                arr[i+1] = 1- arr[i+1];
+                cnt++;
             }
         }
-
-        // 마지막 숫자가 1이 아닐 경우 불가능
-        if (numbers[n - 1] != 1) {
-            System.out.println(-1);
-            return;
+        if(arr.length>=2 && arr[n-2] == 0) {
+            arr[n-2] = 1 - arr[n-2];
+            arr[n-1] = 1 - arr[n-1];
+            cnt++;
         }
-        System.out.println(count);
+        if(arr[n-1]==0) {
+            System.out.println("-1");
+        } else {
+            System.out.println(cnt);
+        }
     }
 }
