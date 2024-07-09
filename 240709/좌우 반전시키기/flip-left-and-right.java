a4ad107d7f40ee80429dc9d54b9458141b7a14c0
PKafka <66552990+PKafka0320@users.noreply.github.com>
2024-07-09 10:59:24 +0900

    Title: 좌우 반전시키기, Time: 91ms, Memory: 8MB, Status: Passed - CodeTree

--- 240709/좌우 반전시키기/flip-left-and-right.java
+++ 240709/좌우 반전시키기/flip-left-and-right.java
@@ -15,11 +15,12 @@ public class Main {
         }
 
         // idx 위치의 숫자가 0일 경우 idx + 1의 숫자와 그 양 옆을 뒤집기
-        for (int idx = 0; idx < n - 2; idx++) {
+        for (int idx = 0; idx < n - 1; idx++) {
             if (numbers[idx] == 0) {
                 count++;
                 numbers[idx] = 1 - numbers[idx];
                 numbers[idx + 1] = 1 - numbers[idx + 1];
+                if (idx == n - 2) continue; // 마지막 숫자는 왼쪽만 뒤집기
                 numbers[idx + 2] = 1 - numbers[idx + 2];
             }
         }
