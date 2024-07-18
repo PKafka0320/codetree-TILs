24d2df8934e71b69df2eac642554b16bd7b3ad2d
PKafka <66552990+PKafka0320@users.noreply.github.com>
2024-07-18 15:30:49 +0900

    Title: 천 개의 정거장, Time: 2997ms, Memory: 135MB, Status: Passed - CodeTree

--- 240718/천 개의 정거장/thousand-stops.java
+++ 240718/천 개의 정거장/thousand-stops.java
@@ -1,139 +1,116 @@
-import java.io.*;
-import java.util.*;
+import java.util.Scanner;
+import java.util.ArrayList;
 
-// 그래프 데이터 클래스
-// 데이터: 도착 지점, 버스번호, 비용
-class Path {
-	int destination, busId;
-	long fare;
-	
-	public Path(int destination, int busId, long fare) {
-		this.destination = destination;
-		this.busId = busId;
-		this.fare = fare;
-	}
-	
-	@Override
-	public String toString() {
-		return destination + ":" + busId;
-	}
-}
+class Pair {
+    long cost, time;
 
-// 최소 비용으로 이동할 수 있는 다음 위치
-// 데이터: 다음 위치, 버스번호, 다음 위치로 이동할 때의 비용
-class Route implements Comparable<Route> {
-	int destination, busId, time;
-	long fare;
-	
-	public Route(int destination, int busId, long fare, int time) {
-		this.destination = destination;
-		this.busId = busId;
-		this.fare = fare;
-		this.time = time;
-	}
-	
-	@Override
-	public int compareTo(Route o) {
-		if (this.fare > o.fare) return 1;
-		else if (this.fare == o.fare) return 0;
-		else return -1;
-	}
-	
-	@Override
-	public String toString() {
-		return destination + ":" + busId + "(" + fare + ")";
-	}
+    public Pair(long cost, long time) {
+        this.cost = cost;
+        this.time = time;
+    }
+
+    public boolean isGreaterThan(Pair p) {
+        return this.cost > p.cost || (this.cost == p.cost && this.time > p.time);
+    }
 }
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    	
-    	// 출발 지점, 도착 지점, 버스의 수 입력
-    	StringTokenizer st = new StringTokenizer(br.readLine());
-    	int startStation = Integer.parseInt(st.nextToken()) - 1;
-    	int endStation = Integer.parseInt(st.nextToken()) - 1;
-    	int totalBusCount = Integer.parseInt(st.nextToken());
-    	
-    	// 인접리스트 그래프 생성 및 데이터 입력
-    	int maxStationIdx = 0;
-    	int[] fares = new int[totalBusCount];
-    	int[][] routes = new int[totalBusCount][];
-    	for (int busId = 0; busId < totalBusCount; busId++) {
-    		st = new StringTokenizer(br.readLine());
-    		fares[busId] = Integer.parseInt(st.nextToken());
-    		int routeCount = Integer.parseInt(st.nextToken());
-    		
-    		routes[busId] = new int[routeCount];
-    		st = new StringTokenizer(br.readLine());
-    		for (int count = 0; count < routeCount; count++) {
-    			routes[busId][count] = Integer.parseInt(st.nextToken()) - 1;
-    			maxStationIdx = Math.max(maxStationIdx, routes[busId][count]);
-    		}
-    	}
-    	
-		ArrayList<Path>[] graph = new ArrayList[maxStationIdx + 1];
-    	for (int idx = 0; idx <= maxStationIdx; idx++) {
-    		graph[idx] = new ArrayList<>();
-    	}
-    	
-    	for (int busId = 0; busId < totalBusCount; busId++) {
-    		int recentStation = routes[busId][0];
-    		for (int station = 1; station < routes[busId].length; station++) {
-    			int nextStation = routes[busId][station];
-    			graph[nextStation].add(new Path(recentStation, busId, fares[busId]));
-    			recentStation = nextStation;
-    		}
-    	}
+    public static final int INT_MAX = Integer.MAX_VALUE;
+    public static final long INF = (long)1e17 + 1;
+    public static final int MAX_M = 1000;
+    
+    // 변수 선언
+    public static int a, b, n, m = 1000;
+    public static Pair[][] graph = new Pair[MAX_M + 1][MAX_M + 1]; // (비용, 시간)을 기록
+    public static boolean[] visited = new boolean[MAX_M + 1];
+    
+    public static Pair[] dist = new Pair[MAX_M + 1];               // (비용, 시간)을 기록
 
-    	// 도착 지점에서 각 지점으로 갈 수 있는 최소 비용 배열 생성 및 초기화
-    	long[] minFares = new long[maxStationIdx + 1];
-    	Arrays.fill(minFares, -1);
-    	minFares[endStation] = 0;
-    	
-    	// 도착 지점에서 각 지점으로 갈 수 있는 최단 비용에 따른 시간 배열 생성 및 초기화
-    	int[] times = new int[maxStationIdx + 1];
-    	Arrays.fill(times, -1);
-    	times[endStation] = 0;
-    	
-    	// 현재 타고 있는 버스와 비교하면서 다음 위치로의 최소 비용 및 버스 번호 갱신
-    	PriorityQueue<Route> pq = new PriorityQueue<>();
-    	pq.add(new Route(endStation, -1, 0, 0));
-    	
-    	while (!pq.isEmpty()) {
-    		Route currentRoute = pq.poll();
-    		int currentStation = currentRoute.destination;
-    		int currentBusId = currentRoute.busId;
-    		long currentFare = currentRoute.fare;
-    		int currentTime = currentRoute.time;
-    		
-    		if (currentFare != minFares[currentStation]) continue;
-    		
-    		for (Path path : graph[currentStation]) {
-    			int nextStation = path.destination;
-    			int nextBusId = path.busId;
-    			long nextFare = path.fare;
-    			
-    			if (minFares[nextStation] == -1) {
-    				if (currentBusId == nextBusId) {
-    					minFares[nextStation] = currentFare;
-    				} else {
-    					minFares[nextStation] = currentFare + nextFare;
-    				}
-					times[nextStation] = currentTime + 1;
-    				pq.add(new Route(nextStation, nextBusId, minFares[nextStation], currentTime + 1));
-    			} else if (minFares[nextStation] > minFares[currentStation]) {
-    				if (currentBusId == nextBusId && minFares[nextStation] > minFares[currentStation]) {
-    					minFares[nextStation] = currentFare;
-    				} else if (currentBusId != nextBusId && minFares[nextStation] > minFares[currentStation] + nextFare) {
-    					minFares[nextStation] = currentFare + nextFare;
-    				}
-					times[nextStation] = currentTime + 1;
-    				pq.add(new Route(nextStation, nextBusId, minFares[nextStation], currentTime + 1));
-    			}
-    		}
-    	}
-    	
-    	System.out.println(minFares[startStation] + " " + times[startStation]);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // 입력
+        a = sc.nextInt();
+        b = sc.nextInt();
+        n = sc.nextInt();
+
+        // 초기 graph 값에 전부 큰 값 기록
+        for(int i = 1; i <= m; i++) {
+            for(int j = 1; j <= m; j++) {
+                graph[i][j] = new Pair(INF, 0);
+            }
+            // 자기 자신은 비용과 시간이 전혀 소요되지 않음
+            graph[i][i] = new Pair(0, 0);
+        }
+
+        // 그래프를 인접행렬로 표현
+        for(int i = 1; i <= n; i++) {
+            int cost = sc.nextInt();
+            int stopNum = sc.nextInt();
+            
+            ArrayList<Integer> stops = new ArrayList<>();
+            for(int j = 0; j < stopNum; j++) {
+                int x = sc.nextInt();
+                stops.add(x);
+            }
+
+            for(int j = 0; j < stopNum; j++) {
+                for(int k = j + 1; k < stopNum; k++) {
+                    Pair newP = new Pair(cost, k - j);
+                    if(graph[stops.get(j)][stops.get(k)].isGreaterThan(newP))
+                        graph[stops.get(j)][stops.get(k)] = newP;
+                }
+            }
+        }
+        
+        // 그래프에 있는 모든 노드들에 대해
+        // 초기값을 전부 아주 큰 값으로 설정
+        // INT_MAX 그 자체로 설정하면
+        // 후에 덧셈 진행시 overflow가 발생할 수도 있으므로
+        // 적당히 큰 숫자로 적어줘야함에 유의!
+        for(int i = 1; i <= m; i++)
+            dist[i] = new Pair(INF, 0);
+
+        // 시작위치에는 dist값을 0으로 설정
+        dist[a] = new Pair(0, 0);
+
+        // O(|V|^2) 다익스트라 코드
+        for(int i = 1; i <= m; i++) {
+            // V개의 정점 중 
+            // 아직 방문하지 않은 정점 중
+            // dist값이 가장 작은 정점을 찾아줍니다.
+            int minIndex = -1;
+            for(int j = 1; j <= m; j++) {
+                if(visited[j])
+                    continue;
+                
+                if(minIndex == -1 || dist[minIndex].isGreaterThan(dist[j]))
+                    minIndex = j;
+            }
+
+            // 최솟값에 해당하는 정점에 방문 표시를 진행합니다.
+            visited[minIndex] = true;
+
+            long minCost = dist[minIndex].cost;
+            long minTime = dist[minIndex].time;
+
+            // 최솟값에 해당하는 정점에 연결된 간선들을 보며
+            // 시작점으로부터의 최단거리 값을 갱신해줍니다.
+            for(int j = 1; j <= m; j++) {
+                long cost = graph[minIndex][j].cost;
+                long time = graph[minIndex][j].time;
+                Pair newP = new Pair(minCost + cost, minTime + time);
+                if(dist[j].isGreaterThan(newP))
+                    dist[j] = newP;
+            }
+        }
+
+        // 만약 도달이 불가능하다면 -1 -1이 답이 됩니다.
+        if(dist[b].cost == INF)
+            dist[b] = new Pair(-1, -1);
+        
+        long minCost = dist[b].cost;
+        long minTime = dist[b].time;
+
+        System.out.print(minCost + " " + minTime);
     }
 }
