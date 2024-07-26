3580f33acbf51ccedf6fbf3a9d057b8f1b364230
PKafka <66552990+PKafka0320@users.noreply.github.com>
2024-07-26 11:07:01 +0900

    Title: 간선의 길이를 2배 늘리기, Time: 424ms, Memory: 27MB, Status: Passed - CodeTree

--- 240726/간선의 길이를 2배 늘리기/double-the-length-of-the-edge.java
+++ 240726/간선의 길이를 2배 늘리기/double-the-length-of-the-edge.java
@@ -1,136 +1,124 @@
-import java.io.*;
-import java.util.*;
-
-class Path {
-	int node, distance;
-	
-	public Path(int node, int distance) {
-		this.node = node;
-		this.distance = distance;
-	}
-}
-
-class Route implements Comparable<Route> {
-	int node, distance;
-	
-	public Route(int node, int distance) {
-		this.node = node; 
-		this.distance = distance;
-	}
-	
-	@Override
-	public int compareTo(Route o) {
-		return this.distance - o.distance;
-	}
-}
+import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Main {
-	static int n, m;
-	static ArrayList<Path>[] graph;
-	static int[] distance, beforeNode;
-	
-    public static void main(String[] args) throws Exception {
-    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    	StringTokenizer st;
-    	
-    	st = new StringTokenizer(br.readLine());
-    	n = Integer.parseInt(st.nextToken());
-    	m = Integer.parseInt(st.nextToken());
-    	
-    	graph = new ArrayList[n];
-    	for (int i = 0; i < n; i++) {
-    		graph[i] = new ArrayList<>();
-    	}
-    	
-    	for (int i = 0; i < m; i++) {
-			st = new StringTokenizer(br.readLine());
-			int node1 = Integer.parseInt(st.nextToken()) - 1;
-			int node2 = Integer.parseInt(st.nextToken()) - 1;
-			int distance = Integer.parseInt(st.nextToken());
-			
-			graph[node1].add(new Path(node2, distance));
-			graph[node2].add(new Path(node1, distance));
-		}
-
-    	dijkstra();
-    	
-    	int originDistance = distance[n - 1];
-    	
-    	List<Integer> list = new ArrayList<>();
-    	int currentNode = n - 1;
-    	while (currentNode != 0) {
-    		list.add(currentNode);
-    		currentNode = beforeNode[currentNode];
-    	}
-
-    	int maxDiff = 0;
-    	for (int i = list.size() - 1; i >= 1; i--) {
-    		int x = list.get(i);
-    		int y = list.get(i - 1);
-
-    		Path originPath1 = null;
-    		Path originPath2 = null;
-    		for (Path path : graph[x]) {
-    			if (path.node == y) {
-    				originPath1 = path;
-    			}
-    		}
-    		for (Path path : graph[y]) {
-    			if (path.node == x) {
-    				originPath2 = path;
-    			}
-    		}
-    		
-    		graph[x].remove(originPath1);
-    		graph[y].remove(originPath2);
-    		
-    		Path tempPath1 = new Path(y, originPath1.distance * 2);
-    		Path tempPath2 = new Path(x, originPath2.distance * 2);
-    		
-    		graph[x].add(tempPath1);
-    		graph[y].add(tempPath2);
-    		
-    		dijkstra();
-    		
-    		maxDiff = Math.max(maxDiff, distance[n - 1] - originDistance);
-    		
-    		graph[x].remove(tempPath1);
-    		graph[y].remove(tempPath2);
-    		graph[x].add(originPath1);
-    		graph[y].add(originPath2);
-		}
-    	
-    	System.out.println(maxDiff);
-    }
+    public static final int INT_MAX = Integer.MAX_VALUE;
+    public static final int MAX_N = 1000;
+    
+    // 변수 선언
+    public static int n, m;
+    public static int[][] graph = new int[MAX_N + 1][MAX_N + 1];
+    public static boolean[] visited = new boolean[MAX_N + 1];
+    
+    public static int[] dist = new int[MAX_N + 1];
+    public static int[] path = new int[MAX_N + 1];
+    
+    public static int ans;
     
     public static void dijkstra() {
-    	distance = new int[n];
-    	Arrays.fill(distance, -1);
-    	distance[0] = 0;
-    	beforeNode = new int[n];
-    	Arrays.fill(beforeNode, -1);
-    	beforeNode[0] = 0;
-    	PriorityQueue<Route> pq = new PriorityQueue<>();
-    	pq.add(new Route(0, 0));
-    	
-    	while (!pq.isEmpty()) {
-    		Route currentRoute = pq.poll();
-    		int currentNode = currentRoute.node;
-    		int currentDistance = currentRoute.distance;
-    		
-    		if (distance[currentNode] != currentDistance) continue;
-    		
-    		for (Path path : graph[currentNode]) {
-    			int nextNode = path.node;
-    			int nextDistance = path.distance;
-    			int sumDistance = currentDistance + nextDistance;
-    			
-    			if (distance[nextNode] == -1 || distance[nextNode] > sumDistance) {
-    				distance[nextNode] = sumDistance;
-    				beforeNode[nextNode] = currentNode;
-    				pq.add(new Route(nextNode, sumDistance));
-    			}
-    		}
-    	}
+        // 그래프에 있는 모든 노드들에 대해
+        // 초기값을 전부 아주 큰 값으로 설정
+        // INT_MAX 그 자체로 설정하면
+        // 후에 덧셈 진행시 overflow가 발생할 수도 있으므로
+        // 적당히 큰 숫자로 적어줘야함에 유의!
+        for(int i = 1; i <= n; i++)
+            dist[i] = (int)1e9;
+    
+        // 시작위치에는 dist값을 0으로 설정
+        dist[1] = 0;
+    
+        // visited 값을 초기화해줍니다.
+        for(int i = 1; i <= n; i++)
+            visited[i] = false;
+    
+        // O(|V|^2) 다익스트라 코드
+        for(int i = 1; i <= n; i++) {
+            // V개의 정점 중 
+            // 아직 방문하지 않은 정점 중
+            // dist값이 가장 작은 정점을 찾아줍니다.
+            int minIndex = -1;
+            for(int j = 1; j <= n; j++) {
+                if(visited[j])
+                    continue;
+                
+                if(minIndex == -1 || dist[minIndex] > dist[j])
+                    minIndex = j;
+            }
+    
+            // 최솟값에 해당하는 정점에 방문 표시를 진행합니다.
+            visited[minIndex] = true;
+    
+            // 최솟값에 해당하는 정점에 연결된 간선들을 보며
+            // 시작점으로부터의 최단거리 값을 갱신해줍니다.
+            for(int j = 1; j <= n; j++) {
+                // 간선이 존재하지 않는 경우에는 넘어갑니다.
+                if(graph[minIndex][j] == 0)
+                    continue;
+    
+                if(dist[j] > dist[minIndex] + graph[minIndex][j]) {
+                    dist[j] = dist[minIndex] + graph[minIndex][j];
+                    // path값을 갱신해줍니다.
+                    path[j] = minIndex;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // 입력
+        n = sc.nextInt();
+        m = sc.nextInt();
+
+        // 그래프를 인접행렬로 표현
+        // 양방향 그래프이므로 양쪽 다 표시해줍니다.
+        while(m-- > 0) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            int z = sc.nextInt();
+            graph[x][y] = z;
+            graph[y][x] = z;
+        }
+        
+        dijkstra();
+
+        // 도착지에서 시작하여
+        // 시작점가 나오기 전까지
+        // path를 따라 움직여줍니다.
+        int idx = n;
+        ArrayList<Integer> vertices = new ArrayList<>();
+        vertices.add(idx);
+        while(idx != 1) {
+            idx = path[idx];
+            vertices.add(idx);
+        }
+
+        // 초기 최단거리를 기록합니다.
+        int origDist = dist[n];
+
+        // 최단거리에 해당하는
+        // 간선을 하나 잡아 2배로 한 뒤
+        // 최단거리를 구해봅니다.
+        // 그 중 최대로 변한 정도를 갱신해줍니다.
+        for(int i = vertices.size() - 1; i >= 1; i--) {
+            // 가중치를 2배로 만들어줍니다.
+            int x = vertices.get(i);
+            int y = vertices.get(i - 1);
+            graph[x][y] *= 2;
+            graph[y][x] *= 2;
+
+            // 최단거리를 구해줍니다.
+            dijkstra();
+
+            // 기존 최단거리와의 차이 중
+            // 최댓값을 갱신해줍니다.
+            ans = Math.max(ans, dist[n] - origDist);
+
+            // 간선 가중치를 원상복구 시킵니다.
+            graph[x][y] /= 2;
+            graph[y][x] /= 2;
+        }
+
+        System.out.print(ans);
     }
 }
