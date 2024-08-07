8f64b9523e2867f2ab1ac331c206c5653c35a221
PKafka <66552990+PKafka0320@users.noreply.github.com>
2024-08-08 08:53:27 +0900

    Title: 트리의 부모 노드, Time: 104ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240808/트리의 부모 노드/parent-node-of-the-tree.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        
+        int[] parents = new int[n];
+        
+        for (int i = 0; i < 11; i++) {
+            st = new StringTokenizer(br.readLine());
+            int parent = Integer.parseInt(st.nextToken()) - 1;
+            int child = Integer.parseInt(st.nextToken()) - 1;
+            
+            parents[child] = parent;
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i < n; i++) {
+            sb.append(parents[i] + 1).append("\n");
+        }
+        
+        System.out.println(sb);
+    }
+
+}
