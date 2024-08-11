c35a15f8fbe4a3d301ce9e06397e73ea108fa7ee
PKafka <66552990+PKafka0320@users.noreply.github.com>
2024-08-11 14:56:28 +0900

    Title: 트리 판별, Time: 112ms, Memory: 9MB, Status: Wrong Answer - CodeTree

--- 240811/트리 판별/tree-identification.java
+++ 240811/트리 판별/tree-identification.java
@@ -2,17 +2,17 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static List<Integer>[] reverseTree;
-	static List<Integer> root;
+	static int N = 10_000;
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		
 		int n = Integer.parseInt(br.readLine()); // 노드의 개수
-		reverseTree = new ArrayList[10_000]; // [i]: i번 노드의 부모 노드 리스트
+		List<Integer>[] reverseTree = new ArrayList[10_000]; // [i]: i번 노드의 부모 노드 리스트
+		Set<Integer> nodeSet = new HashSet<>(); // 언급된 노드 번호
 		
-		for (int idx = 0; idx < 10_000; idx++) {
+		for (int idx = 0; idx < N; idx++) {
 			reverseTree[idx] = new ArrayList<>();
 		}
 		
@@ -22,15 +22,17 @@ public class Main {
 			int to = Integer.parseInt(st.nextToken()) - 1;
 			
 			reverseTree[to].add(from); // 부모 노드 추가
+			nodeSet.add(from);
+			nodeSet.add(to);
 		}
 		
 		int rootCnt = 0;
-		for (int nodeIdx = 0; nodeIdx < n; nodeIdx++) {
+		for (int nodeIdx = 0; nodeIdx < N; nodeIdx++) {
 			if (reverseTree[nodeIdx].size() > 1) {
 				System.out.println(0);
 				return;
 			}
-			if (reverseTree[nodeIdx].size() == 0) {
+			if (reverseTree[nodeIdx].size() == 0 && nodeSet.contains(nodeIdx)) {
 				rootCnt++;
 			}
 		}
@@ -38,6 +40,7 @@ public class Main {
 		if (rootCnt == 1) {
 			System.out.println(1);
 		} else {
+			System.out.println(rootCnt);
 			System.out.println(0);
 		}
 	}
