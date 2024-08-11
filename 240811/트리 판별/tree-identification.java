8d6356b260ed0eb6d85dd100039989d88edccf4b
PKafka <66552990+PKafka0320@users.noreply.github.com>
2024-08-11 15:23:42 +0900

    Title: 트리 판별, Time: 215ms, Memory: 14MB, Status: Passed - CodeTree

--- 240811/트리 판별/tree-identification.java
+++ 240811/트리 판별/tree-identification.java
@@ -1,55 +1,93 @@
-import java.io.*;
-import java.util.*;
+import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Main {
-	static int N = 10_000;
+    public static final int MAX_N = 10000;
+    
+    // 변수 선언:
+    public static int m;
+    public static int root;
+    public static int[] deg = new int[MAX_N + 1];
+    public static ArrayList<Integer>[] edges = new ArrayList[MAX_N + 1];
+    public static boolean[] used = new boolean[MAX_N + 1];
+    public static boolean[] visited = new boolean[MAX_N + 1];
+    public static boolean isTree = true;
+    
+    // DFS를 통해 루트로부터 갈 수 있는 모든 정점을 탐색합니다.
+    public static void dfs(int x) {
+        for(int i = 0; i < edges[x].size(); i++) {
+            int y = edges[x].get(i);
+            // 이미 방문한 노드는 스킵합니다.
+            if(visited[y]) 
+                continue;
+            
+            visited[y] = true;
+            dfs(y);
+        }
+    
+        return;
+    }
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		
-		int n = Integer.parseInt(br.readLine()); // 노드의 개수
-		List<Integer>[] reverseTree = new ArrayList[10_000]; // [i]: i번 노드의 부모 노드 리스트
-		Set<Integer> nodeSet = new HashSet<>(); // 언급된 노드 번호
-		Set<Integer> visited = new HashSet<>(); // 방문한 노드 번호
-		
-		for (int idx = 0; idx < N; idx++) {
-			reverseTree[idx] = new ArrayList<>();
-		}
-		
-		for (int idx = 0; idx < n; idx++) {
-			st = new StringTokenizer(br.readLine());
-			int from = Integer.parseInt(st.nextToken()) - 1;
-			int to = Integer.parseInt(st.nextToken()) - 1;
-			
-			reverseTree[to].add(from); // 부모 노드 추가
-			nodeSet.add(from);
-			nodeSet.add(to);
-		}
-		
-		int rootCnt = 0;
-		for (int nodeIdx = 0; nodeIdx < N; nodeIdx++) {
-			if (reverseTree[nodeIdx].size() > 1) {
-				System.out.println(0);
-				return;
-			}
-			
-			visited.add(nodeIdx);
-			if (reverseTree[nodeIdx].size() == 1 && visited.contains(reverseTree[nodeIdx].get(0))) {
-				System.out.println(0);
-				return;
-			}
-			
-			if (reverseTree[nodeIdx].size() == 0 && nodeSet.contains(nodeIdx)) {
-				rootCnt++;
-			}
-		}
-		
-		if (rootCnt == 1) {
-			System.out.println(1);
-		} else {
-			System.out.println(0);
-		}
-	}
-	
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        // 입력:
+        m = sc.nextInt();
+
+        for(int i = 1; i <= MAX_N; i++)
+            edges[i] = new ArrayList<>();
+        
+        // n개의 간선 정보를 입력받습니다.
+        for(int i = 1; i <= m; i++) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+
+            // 간선 정보를 인접리스트에 넣어줍니다.
+            edges[x].add(y);
+
+            // 해당 번호가 그래프에 있는 정점 번호인지 판단합니다.
+            used[x] = used[y] = true;
+
+            // 정점 별 들어오는 간선의 개수를 저장합니다.
+            deg[y]++;
+        }
+
+        // 루트 노드를 찾습니다. 들어오는 간선이 하나도 없는 노드가 여러개면 트리가 아닙니다.
+        for(int i = 1; i <= MAX_N; i++) {
+            if(used[i] && deg[i] == 0) {
+                // 이미 선정된 루트가 있다면 
+                // 루트가 여러 개인 것이므로 트리가 아닙니다.
+                if(root != 0) 
+                    isTree = false;
+                root = i;
+            }
+        }
+
+        // 루트 노드가 없으면 트리가 아닙니다.
+        if(root == 0) 
+            isTree = false;
+        
+        // 루트 노드를 제외한 노드는 모두 들어오는 간선이 1개씩 있습니다. 그렇지 않으면 트리가 아닙니다.
+        for(int i = 1; i <= MAX_N; i++) {
+            if(used[i] && i != root && deg[i] != 1) {
+                isTree = false;
+            }
+        }
+
+        if(isTree && root != 0) {
+            // root 정점으로부터 모든 정점을 갈 수 있는지 판단합니다.
+            visited[root] = true;
+            dfs(root);
+        }
+
+        // root 정점으로부터 탐색해 도달하지 못하는 정점이 있으면 트리가 아닙니다.
+        for(int i = 1; i <= MAX_N; i++) {
+            if(used[i] && !visited[i]) {
+                isTree = false;
+            }
+        }
+
+        if(isTree) System.out.print(1);
+        else System.out.print(0);
+    }
 }
