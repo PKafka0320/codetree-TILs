bd0ef778fe72ae385e5b6c62dee41737e0ff3d68
PKafka <66552990+PKafka0320@users.noreply.github.com>
2024-08-13 20:08:41 +0900

    Title: 트리 파악, Time: 1440ms, Memory: 76MB, Status: Passed - CodeTree

--- 240813/트리 파악/identifying-the-tree.java
+++ 240813/트리 파악/identifying-the-tree.java
@@ -1,49 +1,67 @@
-import java.io.*;
-import java.util.*;
+import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Main {
-    static int sum;
-    static List<Integer>[] tree;
-    static boolean[] visited;
-
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-
-        int n = Integer.parseInt(br.readLine());
-        visited = new boolean[n];
-        tree = new ArrayList[n];
-        for (int i = 0; i < n; i++) {
-            tree[i] = new ArrayList<>();
+    public static final int MAX_N = 100000;
+    
+    // 변수 선언:
+    public static int n;
+    public static ArrayList<Integer>[] edges = new ArrayList[MAX_N + 1];
+    public static boolean[] visited = new boolean[MAX_N + 1];
+    public static int[] depth = new int[MAX_N + 1];
+    
+    // 리프노드 깊이의 총합
+    public static int ans;
+    
+    // DFS를 통해 리프노드와 리프노드의 깊이를 탐색합니다.
+    public static void dfs(int x) {
+        boolean isLeaf = true;
+    
+        for(int i = 0; i < edges[x].size(); i++) {
+            int y = edges[x].get(i);
+            // 이미 방문한 노드는 스킵합니다.
+            if(visited[y]) continue;
+    
+            visited[y] = true;
+    
+            // 하나라도 자식 노드가 있다면 리프 노드가 아닙니다.
+            isLeaf = false;
+            
+            // root로부터의 거리를 갱신합니다.
+            depth[y] = depth[x] + 1;
+    
+            dfs(y);
         }
-
-        for (int i = 0; i < n -1; i++) {
-            st = new StringTokenizer(br.readLine());
-            int parent = Integer.parseInt(st.nextToken()) - 1;
-            int child = Integer.parseInt(st.nextToken()) - 1;
-
-            tree[parent].add(child);
-            tree[child].add(parent);
-        }
-
-        sum = 0;
-        dfs(0);
-
-        if (sum % 2 == 0) {
-            System.out.println(0);
-        } else {
-            System.out.println(1);
-        }
+    
+        // 리프노드라면, 해당 노드의 깊이를 더합니다.
+        if(isLeaf) 
+            ans += depth[x];
     }
 
-    public static void dfs(int node) {
-        visited[node] = true;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // 입력:
+        n = sc.nextInt();
+        
+        for(int i = 1; i <= n; i++)
+            edges[i] = new ArrayList<>();
 
-        for (int child : tree[node]) {
-            if (visited[child]) continue;
-            sum++;
-            visited[child] = true;
-            dfs(child);
+        // n - 1개의 간선 정보를 입력받습니다.
+        for(int i = 1; i <= n - 1; i++) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+
+            // 간선 정보를 인접리스트에 넣어줍니다.
+            edges[x].add(y);
+            edges[y].add(x);
         }
+
+        // DFS를 통해 리프노드와 리프노드의 깊이를 탐색합니다.
+        visited[1] = true;
+        dfs(1);
+
+        // 모든 리프노드의 깊이의 합이 짝수인지 홀수인지 판단해 정답을 출력합니다.
+        if(ans % 2 == 1) System.out.print(1);
+        else System.out.print(0);
     }
 }
