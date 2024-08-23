c6b73d39bf1f6d8b9eeb66a022d60615fa477477
PKafka <66552990+PKafka0320@users.noreply.github.com>
2024-08-23 09:51:00 +0900

    Title: 트리에서 공 떨어뜨리기, Time: 437ms, Memory: 31MB, Status: Passed - CodeTree

--- 240823/트리에서 공 떨어뜨리기/drop-a-ball-in-a-tree.java
+++ 240823/트리에서 공 떨어뜨리기/drop-a-ball-in-a-tree.java
@@ -52,3 +52,64 @@ public class Main {
     	}
     }
 }
+
+// import java.util.Scanner;
+
+// public class Main {
+//     public static final int MAX_N = 200000;
+    
+//     // 변수 선언
+//     public static int n;
+//     public static long k;
+//     public static int[] leftNum = new int[MAX_N + 1];
+//     public static int[] rightNum = new int[MAX_N + 1];
+//     public static int ans;
+    
+//     // 트리의 루트 노드에서부터 시작해
+//     // k번째 공이 어느 노드로 내려갈지 DFS로 계산합니다.
+//     public static void dfs(int x, long ballNum) {
+//         // 리프 노드에 도착하면 해당 위치가 답이 됩니다.
+//         if(leftNum[x] == -1 && rightNum[x] == -1) {
+//             ans = x;
+//             return;
+//         }
+    
+//         // 왼쪽 노드가 비어있다면 오른쪽으로 이동합니다.
+//         if(leftNum[x] == -1)
+//             dfs(rightNum[x], ballNum);
+//         // 오른쪽 노드가 비어있다면 왼쪽으로 이동합니다.
+//         else if(rightNum[x] == -1)
+//             dfs(leftNum[x], ballNum);
+//         // 홀수 개의 공이 남았다면
+//         // 왼쪽으로 1개 더 공이 떨어지게 되며
+//         // 우리는 왼쪽으로 이동해야 합니다.
+//         else if(ballNum % 2 == 1)
+//             dfs(leftNum[x], (ballNum + 1) / 2);
+//         // 짝수 개의 공이 남았다면
+//         // 왼쪽, 오른쪽 공이 동일하게 떨어지게 되며
+//         // 우리는 오른쪽으로 이동해야 합니다.
+//         else
+//             dfs(rightNum[x], ballNum / 2);
+//     }
+
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         // 입력:
+//         n = sc.nextInt();
+
+//         // 각 노드의 왼쪽 자식노드와 오른쪽 자식노드를 입력받습니다.
+//         for(int i = 1; i <= n; i++) {
+//             leftNum[i] = sc.nextInt();
+//             rightNum[i] = sc.nextInt();
+//         }
+
+//         k = sc.nextLong();
+
+//         // 트리의 루트 노드에서부터 시작해
+//         // k번째 공이 어느 노드로 내려갈지 DFS로 계산합니다.
+//         dfs(1, k);
+
+//         // 공이 마지막에 멈추는 노드의 번호를 출력합니다.
+//         System.out.print(ans);
+//     }
+// }
