209521df29a1bd0239929d774c0fccf413c48104
PKafka <66552990+PKafka0320@users.noreply.github.com>
2024-08-29 08:37:27 +0900

    Title: 중앙 노드, Time: 670ms, Memory: 42MB, Status: Wrong Answer - CodeTree

---
+++ 240829/중앙 노드/median-node.java
@@ -0,0 +1,72 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static int n, r, central;
+	static int[] parents, dp;
+	static boolean[] visited;
+	static List<Integer>[] edges;
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		r = Integer.parseInt(st.nextToken());
+		
+		parents = new int[n + 1];
+		edges = new ArrayList[n + 1];
+		dp = new int[n + 1];
+		visited= new boolean[n + 1];
+		for (int i = 1; i <= n; i++) {
+			edges[i] = new ArrayList<>();
+		}
+		
+		for (int i = 0; i < n - 1; i++) {
+			st = new StringTokenizer(br.readLine());
+			int node1 = Integer.parseInt(st.nextToken());
+			int node2 = Integer.parseInt(st.nextToken());
+			
+			edges[node1].add(node2);
+			edges[node2].add(node1);
+		}
+		
+		central = - 1;
+		visited[r] = true;
+		dfs(r);
+
+		if (central == -1) {
+			System.out.println(0);
+			return;
+		}
+		
+		int max = 0;
+		int min = Integer.MAX_VALUE;
+		for (int child : edges[central]) {
+			max = Math.max(max, dp[child]);
+			min = Math.min(min, dp[child]);
+		}
+		System.out.println(max - min);
+	}
+	
+	public static void dfs(int node) {
+		if (central == -1 &&
+				((node == r && edges[node].size() > 1) ||
+					(node != r && edges[node].size() > 2))) {
+			central = node;
+		}
+		for (int ad : edges[node]) {
+			if (visited[ad]) continue;
+			visited[ad] = true;
+			parents[ad] = node;
+			dfs(ad);
+		}
+		
+		dp[node] = 1;
+		for (int ad : edges[node]) {
+			if (parents[node] == ad) continue;
+			dp[node] += dp[ad];
+		}
+	}
+}
