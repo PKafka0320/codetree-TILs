8658e75b8c713a2af611cd5387eb5730c895e26d
PKafka <66552990+PKafka0320@users.noreply.github.com>
2024-08-29 09:01:23 +0900

    Title: 중앙 노드, Time: 870ms, Memory: 53MB, Status: Passed - CodeTree

--- 240829/중앙 노드/median-node.java
+++ 240829/중앙 노드/median-node.java
@@ -83,3 +83,110 @@ public class Main {
 		}
 	}
 }
+
+// import java.util.Scanner;
+// import java.util.ArrayList;
+
+// public class Main {
+//     public static final int MAX_N = 100000;
+    
+//     // 변수 선언:
+//     public static int n, r;
+//     public static int midNode;
+//     public static ArrayList<Integer>[] edges = new ArrayList[MAX_N + 1];
+//     public static boolean[] visited = new boolean[MAX_N + 1];
+//     public static int[] treeSize = new int[MAX_N + 1];
+//     public static int maxSize, minSize = MAX_N;
+    
+//     // DFS를 통해 중앙 노드를 판단해줍니다.
+//     public static void findMidNode(int x) {
+//         // 자식 노드의 개수는 edge의 개수 - 1입니다. (edge중 하나는 반드시 부모 노드와 연결되어 있으므로)
+//         int child = edges[x].size() - 1;
+    
+//         // 단, 루트 노드 한정으로 부모 노드가 없기 때문에 자식 노드가 1개 더 있습니다.
+//         if(x == r) 
+//             child++;
+    
+//         if(child >= 2 && midNode == 0) {
+//             // 최초로 자식노드가 2개 이상인 노드는 중앙 노드입니다.
+//             midNode = x;
+//         }
+    
+//         if(child == 0 && midNode == 0) {
+//             // 만약 중앙 노드가 없는 상태로 리프 노드로 끝났다면 해당 노드는 중앙 노드입니다.
+//             midNode = x;
+//         }
+    
+//         for(int i = 0; i < edges[x].size(); i++) {
+//             int y = edges[x].get(i);
+    
+//             // 이미 방문한 노드는 스킵합니다.
+//             if(visited[y]) 
+//                 continue;
+            
+//             visited[y] = true;
+//             findMidNode(y);
+//         }
+//     }
+    
+//     // DFS를 통해 각 노드를 루트로 하는 서브트리의 노드의 개수를 계산합니다.
+//     public static void dfs(int x) {
+//         treeSize[x] = 1;
+    
+//         for(int i = 0; i < edges[x].size(); i++) {
+//             int y = edges[x].get(i);
+    
+//             // 이미 방문한 노드는 스킵합니다.
+//             if(visited[y]) 
+//                 continue;
+    
+//             visited[y] = true;
+//             dfs(y);
+    
+//             treeSize[x] += treeSize[y];
+//         }
+//     }
+
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         // 입력:
+//         n = sc.nextInt();
+//         r = sc.nextInt();
+
+//         for(int i = 1; i <= n; i++)
+//             edges[i] = new ArrayList<>();
+        
+//         // n - 1개의 간선 정보를 입력받습니다.
+//         for(int i = 1; i <= n - 1; i++) {
+//             int x = sc.nextInt();
+//             int y = sc.nextInt();
+            
+//             // 간선 정보를 인접리스트에 넣어줍니다.
+//             edges[x].add(y);
+//             edges[y].add(x);
+//         }
+
+//         // 루트 노드로부터 시작해 DFS를 해 정보들을 저장합니다.
+//         visited[r] = true;
+//         findMidNode(r);
+
+//         // visited 배열을 다시 초기화합니다.
+//         for(int i = 1; i <= n; i++) 
+//             visited[i] = false;
+
+//         // DFS를 통해 각 노드를 루트로 하는 서브트리의 노드의 개수를 계산합니다.
+//         visited[midNode] = true;
+//         dfs(midNode);
+
+//         for(int i = 0; i < edges[midNode].size(); i++) {
+//             int y = edges[midNode].get(i);
+
+//             maxSize = Math.max(maxSize, treeSize[y]);
+//             minSize = Math.min(minSize, treeSize[y]);
+//         }
+
+//         // 중앙노드에서 연결된 자식 노드 중 해당 노드로부터 시작한
+//         // 서브트리의 크기가 가장 큰 값에서 작은 값의 차이를 출력합니다.
+//         System.out.print(maxSize - minSize);
+//     }
+// }
