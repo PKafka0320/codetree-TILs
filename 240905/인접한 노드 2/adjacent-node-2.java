74f67ee786f0a10ab234fac05f25490be35079db
PKafka <66552990+PKafka0320@users.noreply.github.com>
2024-09-05 08:55:58 +0900

    Title: 인접한 노드 2, Time: 477ms, Memory: 18MB, Status: Wrong Answer - CodeTree

--- 240905/인접한 노드 2/adjacent-node-2.java
+++ 240905/인접한 노드 2/adjacent-node-2.java
@@ -7,7 +7,6 @@ public class Main{
 	static List<Integer> select;
     
     public static void main(String[] args) throws Exception {
-        System.setIn(new FileInputStream("res/input.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         
