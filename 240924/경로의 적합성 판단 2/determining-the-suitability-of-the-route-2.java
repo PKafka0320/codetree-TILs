be04ecdf7ea81a0152c0ae568f953fc3d551aa96
PKafka <66552990+PKafka0320@users.noreply.github.com>
2024-09-24 09:49:44 +0900

    Title: 경로의 적합성 판단 2, Time: 472ms, Memory: 29MB, Status: Passed - CodeTree

--- 240924/경로의 적합성 판단 2/determining-the-suitability-of-the-route-2.java
+++ 240924/경로의 적합성 판단 2/determining-the-suitability-of-the-route-2.java
@@ -54,3 +54,74 @@ public class Main {
 	}
 
 }
+
+// import java.util.Scanner;
+
+// public class Main {
+//     public static final int MAX_N = 100000;
+    
+//     // 변수 선언
+//     public static int n, m, k;
+    
+//     // 번호별 그룹을 관리합니다.
+//     public static int[] uf = new int[MAX_N + 1];
+//     public static int[] path = new int[MAX_N + 1];
+    
+//     // x의 대표 번호를 찾아줍니다.
+//     public static int find(int x) {
+//         // x가 루트 노드라면 x값을 반환합니다.
+//         if(uf[x] == x)
+//             return x;
+//         // x가 루트 노드가 아니라면
+//         // x의 부모인 uf[x]에서 탐색을 더 진행한 뒤
+//         // 찾아낸 루트 노드를 uf[x]에 넣어줌과 동시에
+//         // 해당 노드값을 반환합니다.
+//         return uf[x] = find(uf[x]);
+//     }
+    
+//     // x, y가 같은 집합이 되도록 합니다.
+//     public static void union(int x, int y) {
+//         // x, y의 대표 번호를 찾은 뒤
+//         // 연결해줍니다.
+//         int X = find(x);
+//         int Y = find(y);
+//         uf[X] = Y;
+//     }
+
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         // 입력:
+//         n = sc.nextInt();
+//         m = sc.nextInt();
+//         k = sc.nextInt();
+
+//         // 초기 uf 값을 설정합니다.
+//         for(int i = 1; i <= n; i++)
+//             uf[i] = i;
+
+//         // 주어진 간선으로
+//         // 연결관계를 만들어줍니다.
+//         while(m-- > 0) {
+//             int a = sc.nextInt();
+//             int b = sc.nextInt();
+
+//             // 합치는 명령입니다.
+//             union(a, b);
+//         }
+
+//         // 경로를 이동하는 것이 가능하면 true, 아니라면 false를 기록합니다.
+//         boolean isPos = true;
+//         for(int i = 1; i <= k; i++)
+//             path[i] = sc.nextInt();
+
+//         // 만약 경로의 i번째 노드에서 i + 1번째 노드가 연결되어 있지 않으면 이동하는 것이 불가능합니다.
+//         // 이는 대표 번호가 동일한지로 판단 가능합니다.
+//         for(int i = 1; i <= k - 1; i++) {
+//             if(find(path[i]) != find(path[i + 1]))
+//                 isPos = false;
+//         }
+        
+//         if(isPos) System.out.print(1);
+//         else System.out.print(0);
+//     }
+// }
