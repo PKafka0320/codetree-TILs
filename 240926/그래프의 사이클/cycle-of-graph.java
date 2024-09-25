eb46ff2ec0a2f0e3cdf5524a786daa4bb3d01936
PKafka <66552990+PKafka0320@users.noreply.github.com>
2024-09-26 08:45:02 +0900

    Title: 그래프의 사이클, Time: 331ms, Memory: 21MB, Status: Passed - CodeTree

---
+++ 240926/그래프의 사이클/cycle-of-graph.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static int N, M, roots[];
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		roots = new int[N+1];
+		
+		for (int i = 1; i <= N; i++) {
+			roots[i] = i;
+		}
+		
+		for (int i = 1; i <= M; i++) {
+			st = new StringTokenizer(br.readLine());
+			
+			int node1 = Integer.parseInt(st.nextToken());
+			int node2 = Integer.parseInt(st.nextToken());
+			
+			if (union(node1, node2)) continue;
+			System.out.println(i);
+			return;
+		}
+		System.out.println("happy");
+	}
+	
+	public static boolean union(int node1, int node2) {
+		int root1 = find(node1);
+		int root2 = find(node2);
+		
+		if (root1 == root2) return false;
+		
+		roots[root2] = root1;
+		return true;
+	}
+	
+	public static int find(int node) {
+		if (roots[node] == node) return node;
+		return roots[node] = find(roots[node]);
+	}
+
+}
