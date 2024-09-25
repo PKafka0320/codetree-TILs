d56372da0d8fa33dd5bad80991b48b34b8d1e7e2
PKafka <66552990+PKafka0320@users.noreply.github.com>
2024-09-26 08:39:49 +0900

    Title: 정점 연결하기, Time: 661ms, Memory: 25MB, Status: Passed - CodeTree

--- 240926/정점 연결하기/connect-vertices.java
+++ 240926/정점 연결하기/connect-vertices.java
@@ -51,3 +51,66 @@ public class Main {
 	}
 
 }
+
+// import java.util.Scanner;
+
+// public class Main {
+//     public static final int MAX_N = 100000;
+    
+//     // 변수 선언
+//     public static int n;
+    
+//     // 번호별 그룹을 관리합니다.
+//     public static int[] uf = new int[MAX_N + 1];
+    
+//     // x의 대표 번호를 찾아줍니다.
+//     public static int find(int x) {
+//         // x가 루트 노드라면 x값을 반환합니다.
+//         if(uf[x] == x)
+//             return x;
+//         // x가 루트 노드가 아니라면
+//         // x의 부모인 uf[x]에서 탐색을 더 진행한 뒤
+//         // 찾아낸 루트 노드를 uf[x]에 넣어줌과 동시에
+//         // 해당 노드값을 반환합니다.
+//         return uf[x] = find(uf[x]);
+//     }
+    
+//     // x, y가 같은 집합이 되도록 합니다.
+//     public static void union(int x, int y) {
+//         // x, y의 대표 번호를 찾은 뒤
+//         // 연결해줍니다.
+//         int X = find(x);
+//         int Y = find(y);
+        
+//         uf[X] = Y;
+//     }
+
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         // 입력:
+//         n = sc.nextInt();
+
+//         // 초기 uf 값을 설정합니다.
+//         for(int i = 1; i <= n; i++)
+//             uf[i] = i;
+
+//         for(int i = 1; i <= n - 2; i++) {
+//             int a = sc.nextInt();
+//             int b = sc.nextInt();
+
+//             // 합치는 명령입니다.
+//             union(a, b);
+//         }
+
+//         // n - 2개의 간선을 이었을 시, 그래프는 총 두 개의 컴퍼넌트로 분리됩니다.
+//         // 사전순으로 가장 앞서도록 만들려면, 1번 노드와, 그리고 1번 노드의 컴퍼넌트에 있지 않은
+//         // 가장 작은 노드를 찾으면 됩니다.
+//         for(int i = 2; i <= n; i++) {
+//             int I = find(i);
+//             if(I != find(1)) {
+//                 System.out.print(1 + " " + i);
+//                 break;
+//             }
+//         }
+//     }
+// }
