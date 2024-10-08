c7043a1eddb18d7042bb19b373b14f8dd64731fc
PKafka <66552990+PKafka0320@users.noreply.github.com>
2024-10-08 09:15:25 +0900

    Title: 친구의 키 3, Time: 726ms, Memory: 35MB, Status: Passed - CodeTree

---
+++ 241008/친구의 키 3/height-of-friends-3.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static int N, M, indegree[];
+	static List<Integer> edges[];
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		indegree = new int[N+1];
+		edges = new ArrayList[N+1];
+		
+		for (int i = 1; i <= N; i++) {
+			edges[i] = new ArrayList<>();
+		}
+		
+		for (int i = 1; i <= M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int from = Integer.parseInt(st.nextToken());
+			int to = Integer.parseInt(st.nextToken());
+			
+			edges[from].add(to);
+			indegree[to]++;
+		}
+		
+		Queue<Integer> queue = new PriorityQueue<>();
+		
+		for (int i = 1; i <= N; i++) {
+			if (indegree[i] == 0) queue.add(i);
+		}
+
+		int count = 0;
+		StringBuilder answer = new StringBuilder();
+		while (!queue.isEmpty()) {
+			int cur = queue.poll();
+			answer.append(cur).append(" ");
+			count++;
+			
+			for (int next : edges[cur]) {
+				if (--indegree[next] <= 0) queue.add(next);
+			}
+		}
+
+		System.out.println(count == N ?answer.toString() : -1);
+	}
+}
