664db01b44f6fdb8e0073d5c6b057d742c8f88eb
PKafka <66552990+PKafka0320@users.noreply.github.com>
2024-10-18 09:50:39 +0900

    Title: 최대와 최소의 차, Time: 824ms, Memory: 37MB, Status: Passed - CodeTree

--- 241018/최대와 최소의 차/difference-between-max-and-min.java
+++ 241018/최대와 최소의 차/difference-between-max-and-min.java
@@ -105,3 +105,108 @@ public class Main {
 		return root[node] = find(root[node]);
 	}
 }
+
+/*
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Collections;
+
+class Tuple implements Comparable<Tuple> {
+    int cost, x, y;
+
+    public Tuple(int cost, int x, int y) {
+        this.cost = cost;
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public int compareTo(Tuple t) {
+        return this.cost - t.cost; // 비용 기준 오름차순 정렬을 진행합니다.
+    }
+}
+
+public class Main {
+    public static final int MAX_M = 100000;
+    public static final int MAX_N = 100000;
+    
+    // 변수 선언
+    public static int n, m;
+    
+    public static ArrayList<Tuple> edges = new ArrayList<>();
+    
+    public static int[] uf = new int[MAX_N + 1];
+    
+    public static int find(int x) {
+        if(uf[x] == x)
+            return x;
+        return uf[x] = find(uf[x]);
+    }
+    
+    public static void union(int x, int y) {
+        int X = find(x);
+        int Y = find(y);
+        uf[X] = Y;
+    }
+    
+    public static int mst() {
+        // uf 값을 초기값을 적어줍니다.
+        for(int i = 1; i <= n; i++)
+            uf[i] = i;
+        
+        // cost가 낮은 간선부터 순서대로 보며
+        // 아직 두 노드가 연결이 되어있지 않을 경우에만
+        // 해당 간선을 선택하고 두 노드를 합쳐주면서
+        // mst를 만들어줍니다.
+        int totalCost = 0;
+        for(int i = 0; i < edges.size(); i++) {
+            int cost = edges.get(i).cost;
+            int x = edges.get(i).x;
+            int y = edges.get(i).y;
+    
+            // x, y가 연결되어 있지 않다면
+            if(find(x) != find(y)) {
+                // 해당 간선은 MST에 속하는 간선이므로
+                // 답을 갱신해주고
+                // 두 노드를 연결해줍니다.
+    
+                totalCost += cost;
+                union(x, y);
+            }
+        }
+    
+        return totalCost;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // 입력:
+        n = sc.nextInt();
+        m = sc.nextInt();
+
+        // m개의 간선의 정보를 전부 입력받습니다.
+        for(int i = 1; i <= m; i++) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            int cost = sc.nextInt();
+            edges.add(new Tuple(1 - cost, x, y));
+        }
+
+        // cost 순으로 오름차순 정렬을 진행합니다.
+        Collections.sort(edges);
+
+        // 최소 MST를 구합니다.
+        int ansMin = mst();
+
+        // cost 순으로 내림차순 정렬을 진행합니다.
+        Collections.sort(edges, Collections.reverseOrder());
+
+        // 최대 MST를 구합니다.
+        int ansMax = mst();
+        
+        // 정답은 최대 비용으로 만든 mst에서 최소 비용으로 만든 mst의 비용을 빼면 됩니다.
+        System.out.print((long)ansMax * ansMax - (long)ansMin * ansMin);
+    }
+}
+*/
