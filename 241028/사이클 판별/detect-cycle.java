13667cf58525e1a4e5c7b4353e06580197deaf5a
PKafka <66552990+PKafka0320@users.noreply.github.com>
2024-10-28 16:21:06 +0900

    Title: 사이클 판별, Time: 445ms, Memory: 38MB, Status: Passed - CodeTree

--- 241028/사이클 판별/detect-cycle.java
+++ 241028/사이클 판별/detect-cycle.java
@@ -44,3 +44,88 @@ public class Main {
 	}
 
 }
+
+/*
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Queue;
+import java.util.LinkedList;
+
+public class Main {
+    public static final int MAX_N = 100000;
+    
+    // 변수 선언
+    public static int n, m;
+    
+    public static ArrayList<Integer>[] edges = new ArrayList[MAX_N + 1];
+    
+    // 진입차수를 관리합니다.
+    public static int[] indegree = new int[MAX_N + 1];
+    
+    // 각 정점을 방문했는지 판단합니다.
+    public static boolean[] visited = new boolean[MAX_N + 1];
+    
+    // 위상정렬을 위한 큐를 관리합니다.
+    public static Queue<Integer> q = new LinkedList<>();
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // 입력:
+        n = sc.nextInt();
+        m = sc.nextInt();
+
+        // 인접리스트로 관리합니다.
+        for(int i = 1; i <= n; i++)
+            edges[i] = new ArrayList<>();
+
+        for(int i = 1; i <= m; i++) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+
+            edges[x].add(y); 
+            indegree[y]++; // 진입차수를 갱신합니다.  
+        }
+
+        // 처음 indegree 값이 0인 곳이 시작점이 됩니다.
+        // 이 노드들을 queue에 넣어줍니다.
+        for(int i = 1; i <= n; i++)
+            if(indegree[i] == 0)
+                q.add(i);
+        
+        // 위상정렬을 진행합니다.
+        // queue에 원소가 남아있다면 계속 진행합니다.
+        while(!q.isEmpty()) {
+            // 가장 앞에 있는 원소를 뽑아줍니다.
+            int x = q.poll();
+            
+            visited[x] = true;
+
+            // x에서 갈 수 있는 모든 곳을 탐색합니다.
+            for(int i = 0; i < edges[x].size(); i++) {
+                int y = edges[x].get(i);
+
+                // 해당 노드의 indegree를 1만큼 감소시켜줍니다.
+                indegree[y]--;
+
+                // 비로소 indegree가 0이 되었다면
+                // queue에 새로 넣어줍니다.
+                if(indegree[y] == 0)
+                    q.add(y);
+            }
+        }
+
+        // 모든 노드를 방문했다면 그래프 내에 사이클이 존재하지 않는다는 뜻입니다.
+        boolean isCycle = false;
+        for(int i = 1; i <= n; i++) {
+            if(!visited[i]) 
+                isCycle = true;
+        }
+
+        // 사이클이 존재하는지 여부를 출력합니다.
+        if(isCycle)
+            System.out.print("Exists");
+        else
+            System.out.print("Not Exists");
+    }
+}
+*/
