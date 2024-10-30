b442a4b26e75d05e7529adcec8853ef57ab77287
PKafka <66552990+PKafka0320@users.noreply.github.com>
2024-10-30 16:13:12 +0900

    Title: 친구의 키 4, Time: 1189ms, Memory: 101MB, Status: Passed - CodeTree

--- 241030/친구의 키 4/height-of-friends-4.java
+++ 241030/친구의 키 4/height-of-friends-4.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static int N;
+	static int N, M, indegree[], query[][];
 	static List<Integer> edges[];
 
 	public static void main(String[] args) throws Exception {
@@ -11,51 +11,79 @@ public class Main {
 		
 		st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
 		
-		int[] indegree = new int[N+1];
+		query = new int[M+1][2];
+		indegree = new int[N+1];
 		edges = new ArrayList[N+1];
-		
-		for (int i = 1; i <= N; i++) {
-			edges[i] = new ArrayList<>();
-		}
 		
 		for (int i = 1; i <= M; i++) {
 			st = new StringTokenizer(br.readLine());
 			int from = Integer.parseInt(st.nextToken());
 			int to = Integer.parseInt(st.nextToken());
-			
-			if (edges[from].contains(to)) continue;
-			edges[from].add(to);
-			
-			if (cycle(to)) {
-				System.out.println(i);
-				return;
-			}
+
+			query[i][0] = from;
+			query[i][1] = to;
 		}
-		System.out.println("Consistent");
+		
+		int lo = 1, hi = M;
+		int ans = 0;
+        while (lo <= hi) {
+            int mid = (lo + hi) / 2;
+
+            if(cycleExist(mid)) {
+                ans = mid;
+                hi = mid - 1;
+            }
+            else {
+                lo = mid + 1;
+            }
+        }
+		
+		System.out.println(ans == 0 ? "Consistent" : ans);
 	}
 	
-	public static boolean cycle(int start) {
+	public static boolean cycleExist(int limit) {
 		boolean[] visited = new boolean[N+1];
 		Queue<Integer> queue = new LinkedList<>();
 		
-		queue.add(start);
-		visited[start] = true;
+		for (int i = 1; i <= N; i++) {
+			indegree[i] = 0;
+			edges[i] = new ArrayList<>();
+			visited[i] = false;
+		}
+		
+		for (int i = 1; i <= limit; i++) {
+			int from = query[i][0];
+			int to = query[i][1];
+			
+			edges[from].add(to);
+			indegree[to]++;
+		}
+		
+		for (int i = 1; i <= N; i++) {
+			if (indegree[i] == 0) {
+				queue.add(i);
+			}
+		}
 		
 		while (!queue.isEmpty()) {
 			int cur = queue.poll();
+			visited[cur] = true;
 			
 			for (int next : edges[cur]) {
-				if (visited[next]) {
-					return true;
+				if (--indegree[next] == 0) {
+					queue.add(next);
 				}
-				visited[next] = true;
-				queue.add(next);
+			}
+		}
+		
+		for (int i = 1; i <= N; i++) {
+			if (!visited[i]) {
+				return true;
 			}
 		}
 		
 		return false;
 	}
-
 }
