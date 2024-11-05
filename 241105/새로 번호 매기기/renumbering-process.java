5b2e0b2b550d5e84903e37a8f2a40b3d77261dcd
PKafka <66552990+PKafka0320@users.noreply.github.com>
2024-11-05 11:21:12 +0900

    Title: 새로 번호 매기기, Time: 433ms, Memory: 26MB, Status: Wrong Answer - CodeTree

---
+++ 241105/새로 번호 매기기/renumbering-process.java
@@ -0,0 +1,61 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+
+		st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		int[] indegree = new int[N+1];
+		List<Integer>[] edge = new ArrayList[N+1];
+		
+		for (int i = 1; i <= N; i++) {
+			edge[i] = new ArrayList<>();
+		}
+		
+		while (M-- > 0) {
+			st = new StringTokenizer(br.readLine());
+			int from = Integer.parseInt(st.nextToken());
+			int to = Integer.parseInt(st.nextToken());
+			
+			edge[from].add(to);
+			indegree[to]++;
+		}
+		
+		Queue<Integer> pq = new PriorityQueue<>();
+		
+		for (int i = 1; i <= N; i++) {
+			if (indegree[i] == 0) pq.add(i);
+		}
+		
+		int[] num = new int[N+1];
+		int currentNum = 1;
+		int count = 0;
+		while (!pq.isEmpty()) {
+			int cur = pq.poll();
+			num[cur] = currentNum++;
+			count++;
+			
+			for (int next : edge[cur]) {
+				if (--indegree[next] == 0) {
+					pq.add(next);
+				}
+			}
+		}
+		
+		if (count != N) {
+			System.out.println(-1);
+		} else {
+			StringBuilder answer = new StringBuilder();
+			for (int i = 1; i <= N; i++) {
+				answer.append(num[i]).append(" ");
+			}
+			System.out.println(answer.toString());
+		}
+	}
+}
