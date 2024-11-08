eb2e752a45ca6950c894e32014b24fb8a22093ee
PKafka <66552990+PKafka0320@users.noreply.github.com>
2024-11-08 10:03:33 +0900

    Title: 가장 많이 등장한 부분 문자열, Time: 331ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241108/가장 많이 등장한 부분 문자열/most-common-substring.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+
+		st = new StringTokenizer(br.readLine());
+		int L = Integer.parseInt(st.nextToken());
+		String S = st.nextToken();
+
+		int p = 31, m = (int) 1e9 + 7;
+		int[] pPow = new int[L + 1];
+		pPow[0] = 1;
+		for (int i = 1; i <= L; i++) {
+			pPow[i] = (pPow[i - 1] * p) % m;
+		}
+
+		Map<Long, Integer> counts = new HashMap<>();
+
+		long tH = 0;
+		for (int i = 0; i < L; i++) {
+			long hash = toInt(S.charAt(i)) * pPow[L - i - 1];
+			tH = (tH + hash) % m;
+		}
+		counts.put(tH, counts.getOrDefault(tH, 0) + 1);
+
+		for (int i = L; i < S.length(); i++) {
+			tH = ((tH * p) - (toInt(S.charAt(i - L)) * pPow[L]) + toInt(S.charAt(i))) % m;
+			
+			if (tH < 0) {
+				tH += m;
+			}
+			counts.put(tH, counts.getOrDefault(tH, 0) + 1);
+		}
+		
+		int max = 0;
+		for (long value : counts.keySet()) {
+			max = Math.max(max, counts.get(value));
+		}
+		System.out.println(max);
+	}
+
+	public static int toInt(char ch) {
+		return ch - 'a' + 1;
+	}
+}
