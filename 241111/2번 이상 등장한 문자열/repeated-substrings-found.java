474fffa398336159416f6a1c7820764634588c64
PKafka <66552990+PKafka0320@users.noreply.github.com>
2024-11-11 13:52:46 +0900

    Title: 2번 이상 등장한 문자열, Time: 1182ms, Memory: 75MB, Status: Passed - CodeTree

--- 241111/2번 이상 등장한 문자열/repeated-substrings-found.java
+++ 241111/2번 이상 등장한 문자열/repeated-substrings-found.java
@@ -30,38 +30,46 @@ public class Main {
 		int textLen = text.length();
 		Set<Long> hashes = new HashSet<>();
 
-		int POWER = 31, MOD = (int) 1e9 + 7;
-		long[] patternPower = new long[patternLength + 1];
-		patternPower[0] = 1;
-		for (int i = 1; i <= patternLength; i++) {
-			patternPower[i] = (patternPower[i - 1] * POWER) % MOD;
+		int[] POWER = {31, 37}, MOD = {(int) 1e9 + 7, (int) 1e9 + 9};
+		long[][] patternPower = new long[2][patternLength + 1];
+		patternPower[0][0] = patternPower[1][0] = 1;
+		for (int k = 0; k < 2; k++) {
+			for (int i = 1; i <= patternLength; i++) {
+				patternPower[k][i] = (patternPower[k][i - 1] * POWER[k]) % MOD[k];
+			}
 		}
 
-		long textHash = 0;
-		for (int i = 0; i < patternLength; i++) {
-			long hash = toInt(text.charAt(i)) * patternPower[patternLength - 1 - i];
-			textHash = (textHash + hash) % MOD;
-		}
-		if (textHash < 0) {
-			textHash += MOD;
+		long[] textHash = {0, 0};
+		for (int k = 0; k < 2; k++) {
+			for (int i = 0; i < patternLength; i++) {
+				long hash = toInt(text.charAt(i)) * patternPower[k][patternLength - 1 - i];
+				textHash[k] = (textHash[k] + hash) % MOD[k];
+			}
 		}
 
-		hashes.add(textHash);
+		hashes.add(genUniqueKey(textHash[0], textHash[1], MOD));
 
-		for (int i = patternLength; i < textLen; i++) {
-			textHash = ((textHash * POWER) - (toInt(text.charAt(i - patternLength)) * patternPower[patternLength])
-					+ toInt(text.charAt(i))) % MOD;
-			if (textHash < 0) {
-				textHash += MOD;
+		for (int i = 1; i <= textLen - patternLength; i++) {
+			for (int k = 0; k < 2; k++) {
+				textHash[k] = ((textHash[k] * POWER[k]) - (toInt(text.charAt(i - 1)) * patternPower[k][patternLength])
+						+ toInt(text.charAt(i + patternLength - 1))) % MOD[k];
+				if (textHash[k] < 0) {
+					textHash[k] += MOD[k];
+				}
 			}
 			
-			if (hashes.contains(textHash)) {
+			if (hashes.contains(genUniqueKey(textHash[0], textHash[1], MOD))) {
 				return true;
 			}
+			hashes.add(genUniqueKey(textHash[0], textHash[1], MOD));
 		}
 
 		return false;
 	}
+	
+    public static long genUniqueKey(long h1, long h2, int[] MOD) {
+        return h1 * Math.max(MOD[0], MOD[1]) + h2;
+    }
 
 	public static int toInt(char ch) {
 		return ch - 'a' + 1;
