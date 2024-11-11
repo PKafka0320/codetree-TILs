6c4e41acf041abe967a65b20f40f88d03e24cc3d
PKafka <66552990+PKafka0320@users.noreply.github.com>
2024-11-11 13:53:47 +0900

    Title: 2번 이상 등장한 문자열, Time: 945ms, Memory: 68MB, Status: Passed - CodeTree

--- 241111/2번 이상 등장한 문자열/repeated-substrings-found.java
+++ 241111/2번 이상 등장한 문자열/repeated-substrings-found.java
@@ -75,3 +75,103 @@ public class Main {
 		return ch - 'a' + 1;
 	}
 }
+
+/*
+import java.util.Scanner;
+import java.util.HashSet;
+
+public class Main {
+    public static final int MAX_N = 100000;
+    
+    // 변수 선언
+    public static String s;
+    
+    public static int n;
+    
+    // 2개의 polynomial rolling 해싱을 위한 p, m 값을 정의합니다.
+    public static int[] p = new int[]{31, 37};
+    public static int[] m = new int[]{(int)1e9 + 7, (int)1e9 + 9};
+    
+    // p^i, 값을 m으로 나눈 나머지를 관리합니다.
+    public static long[][] pPow = new long[2][MAX_N + 1];
+    
+    // 소문자 알파벳을 수로 변경합니다.
+    public static int toInt(char c) {
+        return c - 'a' + 1;
+    }
+
+    // (h1, h2) 값으로 unique한 값을 만들어줍니다.
+    public static long genUniqueKey(long h1, long h2) {
+        return h1 * Math.max(m[0], m[1]) + h2;
+    }
+    
+    // 길이가 l인 부분 문자열 중 2번 이상 등장하는 경우가 있는지 확인합니다.
+    public static boolean exists(int l) {
+        // pPow 값을 계산합니다.
+        for(int k = 0; k < 2; k++) {
+            // pPow[i] = p^i % m
+            pPow[k][0] = 1;
+            for(int i = 1; i <= n; i++)
+                pPow[k][i] = (pPow[k][i - 1] * p[k]) % m[k];
+        }
+    
+        HashSet<Long> hs = new HashSet<>();
+    
+        // s에서 구간 [0, l - 1]에 해당하는 해싱값을 계산합니다.
+        long[] h = new long[2];
+        for(int k = 0; k < 2; k++) {
+            for(int i = 0; i < l; i++)
+                h[k] = (h[k] + toInt(s.charAt(i)) * pPow[k][l - 1 - i]) % m[k];
+        }
+    
+        // set에 넣어줍니다.
+        hs.add(genUniqueKey(h[0], h[1]));
+    
+        // [i, i + l - 1] 구간을 부분문자열로 하는 경우를 전부 탐색합니다.
+        for(int i = 1; i <= n - l; i++) {
+            for(int k = 0; k < 2; k++) {
+                // hash 값을 갱신해줍니다.
+                h[k] = (h[k] * p[k] - toInt(s.charAt(i - 1)) * pPow[k][l] + toInt(s.charAt(i + l - 1))) % m[k];
+                // 값을 양수로 변환해줍니다.
+                if(h[k] < 0)
+                    h[k] += m[k];
+            }
+    
+            // 이미 동일한 해싱값이 존재한다면
+            // 2번 이상 등장하는 것입니다.
+            if(hs.contains(genUniqueKey(h[0], h[1])))
+                return true;
+            
+            // set에 넣어줍니다.
+            hs.add(genUniqueKey(h[0], h[1]));
+        }
+    
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // 입력:
+        s = sc.next();
+        n = s.length();
+
+        // Parametric Search를 진행합니다.
+        int l = 1, r = n, ans = 0;
+        while(l <= r) {
+            int mid = (l + r) / 2;
+            // 길이가 mid인 부분 문자열 중 2번 이상 등장하는 경우가 있다면
+            // 답을 갱신해주고
+            // 문자열의 길이를 더 늘려봐도 됩니다.
+            if(exists(mid)) {
+                ans = mid;
+                l = mid + 1;
+            }
+            else 
+                r = mid - 1;
+        }
+
+        System.out.print(ans);
+    }
+}
+*/
