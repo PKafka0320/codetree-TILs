e47d89cd4e48ec8dd0c940cbddd9e40e5e80c8e1
PKafka <66552990+PKafka0320@users.noreply.github.com>
2024-11-18 11:26:57 +0900

    Title: 가장 긴 접두사, Time: 953ms, Memory: 176MB, Status: Memory Limit Exceed - CodeTree

---
+++ 241118/가장 긴 접두사/longest-prefix.java
@@ -0,0 +1,83 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+
+		String text = br.readLine();
+		
+		int result = 1;
+		for (int i = text.length(); i >= 1; i--) {
+			StringBuffer sb = new StringBuffer(text.substring(0, i));
+			String pattern = sb.reverse().toString();
+			
+			int[] failure = makeFailure(pattern);
+			// System.out.printf("pattern : %s%n", pattern);
+			// System.out.printf("failure : %s%n", Arrays.toString(failure));
+			
+			if (kmp(text, pattern, failure)) {
+				result = i;
+				break;
+			}
+		}
+		System.out.println(result);
+		
+//		int left = 1, right = text.length();
+//		int result = 1;
+//		while (left <= right) {
+//			int mid = (left + right) / 2;
+//			
+//			StringBuffer sb = new StringBuffer(text.substring(0, mid));
+//			String pattern = sb.reverse().toString();
+//			
+//			int[] failure = makeFailure(pattern);
+//			System.out.printf("pattern : %s%n", pattern);
+//			System.out.printf("failure : %s%n", Arrays.toString(failure));
+//			
+//			if (kmp(text, pattern, failure)) {
+//				System.out.println("find");
+//				left = mid + 1;
+//				result = mid;
+//			} else {
+//				System.out.println("notfind");
+//				right = mid - 1;
+//			}
+//		}
+
+//		System.out.println(result);
+	}
+	
+	public static boolean kmp(String text, String pattern, int[] failure) {
+		for (int i = 0, j = 0; i < text.length(); i++) {
+			while (j > 0 && text.charAt(i) != pattern.charAt(j)) {
+				j = failure[j - 1];
+			}
+			
+			if (text.charAt(i) == pattern.charAt(j)) {
+				if (++j == pattern.length()) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	public static int[] makeFailure(String text) {
+		int[] result = new int[text.length()];
+
+		for (int i = 1, j = 0; i < text.length(); i++) {
+			while (j > 0 && text.charAt(i) != text.charAt(j)) {
+				j = result[j - 1];
+			}
+
+			if (text.charAt(i) == text.charAt(j)) {
+				result[i] = ++j;
+			}
+		}
+
+		return result;
+	}
+}
