9c4bef081bee7e5495819e076ab2b9fe180f264f
PKafka <66552990+PKafka0320@users.noreply.github.com>
2024-11-19 09:44:56 +0900

    Title: 문자열을 최대로 채워넣기, Time: 335ms, Memory: 27MB, Status: Passed - CodeTree

--- 241119/문자열을 최대로 채워넣기/padding-a-string.java
+++ 241119/문자열을 최대로 채워넣기/padding-a-string.java
@@ -2,73 +2,77 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static boolean[] used;
-	static int answer;
+	static int N, M;
+	static String text;
+	static boolean[][] isPos;
+	static int[] patternLength;
+	static int[] dp;
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 
 		st = new StringTokenizer(br.readLine());
-		String text = st.nextToken();
-		used = new boolean[text.length()];
-		int M = Integer.parseInt(st.nextToken());
-		List<String> patterns = new ArrayList<>(M);
+		text = st.nextToken();
+		N = text.length();
+		text = "#" + text;
+		M = Integer.parseInt(st.nextToken());
+		isPos = new boolean[N + 1][M + 1];
+		patternLength = new int[M + 1];
+		dp = new int[N + 1];
 
 		st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < M; i++) {
-			patterns.add(st.nextToken());
+		for (int i = 1; i <= M; i++) {
+			String pattern = st.nextToken();
+			int patternLen = pattern.length();
+			patternLength[i] = patternLen;
+
+			pattern = "#" + pattern;
+
+			int[] failure = makeFailure(pattern, patternLen);
+
+			kmp(pattern, i, failure, patternLen);
 		}
 
-		patterns.sort(new Comparator<String>() {
-			@Override
-			public int compare(String o1, String o2) {
-				return o1.length() - o2.length();
-			}
-		});
-
-		for (String pattern : patterns) {
-			int[] failure = makeFailure(pattern);
-//			System.out.println(Arrays.toString(failure));
-
-			kmp(text, pattern, failure);
-		}
-
-		System.out.println(answer);
-	}
-
-	public static void kmp(String text, String pattern, int[] failure) {
-		for (int i = 0, j = 0; i < text.length(); i++) {
-			if (used[i]) {
-				j = 0;
-				continue;
-			}
-			while (j > 0 && text.charAt(i) != pattern.charAt(j)) {
-				j = failure[j - 1];
-			}
-
-			if (text.charAt(i) == pattern.charAt(j)) {
-				if (++j == pattern.length()) {
-//					System.out.printf("make true %d ~ %d%n", i - j + 1, i);
-					Arrays.fill(used, i - j + 1, i + 1, true);
-					answer += pattern.length();
-					j = 0;
+		int[] dp = new int[N + 1];
+		for (int i = 1; i <= N; i++) {
+			dp[i] = dp[i - 1];
+			for (int j = 1; j <= M; j++) {
+				if (isPos[i][j]) {
+					dp[i] = Math.max(dp[i], patternLength[j] + dp[i - patternLength[j]]);
 				}
 			}
 		}
+
+		System.out.println(dp[N]);
 	}
 
-	public static int[] makeFailure(String pattern) {
-		int[] failure = new int[pattern.length()];
-
-		for (int i = 1, j = 0; i < pattern.length(); i++) {
-			while (j > 0 && pattern.charAt(i) != pattern.charAt(j)) {
-				j = failure[j - 1];
+	public static void kmp(String pattern, int idx, int[] failure, int patternLen) {
+		int j = 0;
+		for (int i = 1; i <= N; i++) {
+			while (j >= 0 && text.charAt(i) != pattern.charAt(j + 1)) {
+				j = failure[j];
 			}
 
-			if (pattern.charAt(i) == pattern.charAt(j)) {
-				failure[i] = ++j;
+			if (++j == patternLen) {
+				isPos[i][idx] = true;
+				j = failure[j];
 			}
+		}
+	}
+
+	public static int[] makeFailure(String pattern, int patternLen) {
+		int[] failure = new int[patternLen + 1];
+
+		failure[0] = -1;
+		for (int i = 1; i <= patternLen; i++) {
+			int j = failure[i - 1];
+
+			while (j >= 0 && pattern.charAt(i) != pattern.charAt(j + 1)) {
+				j = failure[j];
+			}
+
+			failure[i] = j + 1;
 		}
 
 		return failure;
