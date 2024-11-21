189a1c5190e6a2eec56c14aec307a4c4e2844636
PKafka <66552990+PKafka0320@users.noreply.github.com>
2024-11-22 08:46:20 +0900

    Title: 검색어와 접두사, Time: 227ms, Memory: 16MB, Status: Passed - CodeTree

--- 241122/검색어와 접두사/search-terms-and-prefixes.java
+++ 241122/검색어와 접두사/search-terms-and-prefixes.java
@@ -64,3 +64,96 @@ public class Main {
 		System.out.println(answer.toString());
 	}
 }
+
+/*
+import java.util.Scanner;
+
+// Trie에 사용되는 노드를 정의합니다.
+class TrieNode {
+    // 각 노드에는 'a'부터 'z'까지의 문자에 대응되는 26개의 노드 정보가 관리됩니다.
+    TrieNode[] children = new TrieNode[26];
+    // 해당 노드를 거쳐가는 단어가 몇 개 있는지 관리합니다.
+    int num;
+
+    // 생성자입니다.
+    TrieNode() {
+        // 단어 완성에 대한 초기값은 false입니다.
+        num = 0;
+
+        // 각 문자에 대응되는 노드 정보는 처음에 null이 됩니다.
+        for(int i = 0; i < 26; i++)
+            children[i] = null;
+    }
+};
+
+public class Main {
+    public static final int MAX_N = 100000;
+    
+    // 변수 선언
+    public static int n, m;
+    public static String[] words = new String[MAX_N];
+    
+    // 루트 노드에 해당하는 TrieNode를 처음 만들어줍니다.
+    public static TrieNode root = new TrieNode();
+    
+    // 단어 s를 Trie에 넣어줍니다.
+    public static void insertWord(String s) {
+        // root에서 시작합니다.
+        TrieNode t = root;
+        for(int i = 0; i < s.length(); i++) {
+            // 문자 순서대로 따라가면 됩니다.
+            // 'a'부터 'z'까지 사용되므로
+            // 각각을 A(0)부터 Z(25)까지의 index로 매핑시켜줍니다.
+            int index = s.charAt(i) - 'a';
+            // 해당하는 노드가 아직 없다면 새로운 노드를 만들어줍니다.
+            if(t.children[index] == null)
+                t.children[index] = new TrieNode();
+            
+            // index에 해당하는 노드로 옮겨갑니다.
+            t = t.children[index];
+            t.num++; // 단어를 만드는 중이므로 지나가는 노드마다 1씩 더해줍니다.
+        }
+    }
+    
+    // 단어 s를 탐색하며
+    // 해당 단어를 접두사로 하는 서로 다른 단어의 수를 계속 출력합니다.
+    public static void searchWord(String s) {
+        // root에서 시작합니다.
+        TrieNode t = root;
+        for(int i = 0; i < s.length(); i++) {
+            // 해당하는 노드를 계속 따라갑니다.
+            if(t != null) {
+                int index = s.charAt(i) - 'a';
+                t = t.children[index];
+            }
+    
+            // 한 글자 한 글자 입력 될때마다
+            // 해당 글자를 시작으로 하는 (거쳐가는) 단어의 수를 출력합니다.
+            if(t != null)
+                System.out.print(t.num + " ");
+            // 더 이상 그러한 노드가 없다면 답은 0이 됩니다.
+            else
+                System.out.print("0 ");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // 입력:
+        n = sc.nextInt();
+        m = sc.nextInt();
+        for(int i = 0; i < n; i++)
+            words[i] = sc.next();
+
+        // Trie에 단어들을 넣어줍니다.
+        for(int i = 0; i < n; i++)
+            insertWord(words[i]);
+
+        // 문자열 S를 입력받습니다.
+        String S = sc.next();
+
+        searchWord(S);
+    }
+}
+*/
