ceb07f48e21e581ca061d55d5adaf8b50262fcac
PKafka <66552990+PKafka0320@users.noreply.github.com>
2024-12-03 19:27:10 +0900

    Title: 두 사람과 카드 3, Time: 292ms, Memory: 39MB, Status: Passed - CodeTree

--- 241203/두 사람과 카드 3/two-people-and-cards-3.java
+++ 241203/두 사람과 카드 3/two-people-and-cards-3.java
@@ -1,3 +1,72 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int N = Integer.parseInt(st.nextToken());
+		int[] numbers = new int[N + 1];
+		int M = Integer.parseInt(st.nextToken());
+
+		st = new StringTokenizer(br.readLine());
+		for (int i = 1; i <= N; i++) {
+			numbers[i] = Integer.parseInt(st.nextToken());
+		}
+
+		int[][] diff = new int[N + 1][N + 1];
+
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= N; j++) {
+				diff[i][j] = Math.abs(numbers[i] - numbers[j]);
+			}
+		}
+
+		long[][][][] dp = new long[N + 1][N + 1][N + 1][M + 1];
+		for (int i = 0; i <= N; i++) {
+			for (int j = 0; j <= N; j++) {
+				for (int k = 0; k <= N; k++) {
+					for (int m = 0; m <= M; m++) {
+						dp[i][j][k][m] = (long) 1e12;
+					}
+				}
+			}
+		}
+		dp[0][0][0][0] = 0;
+
+		for (int i = 0; i <= N; i++) {
+			for (int j = 0; j <= N; j++) {
+				for (int k = 0; k <= N; k++) {
+					for (int m = 0; m <= M; m++) {
+						int next = i + 1;
+
+						if (next == N + 1)
+							continue;
+
+						dp[next][next][k][m] = Math.min(dp[next][next][k][m], dp[i][j][k][m] + diff[j][next]);
+						dp[next][j][next][m] = Math.min(dp[next][j][next][m], dp[i][j][k][m] + diff[k][next]);
+
+						if (m == M)
+							continue;
+
+						dp[next][j][k][m + 1] = Math.min(dp[next][j][k][m + 1], dp[i][j][k][m]);
+					}
+				}
+			}
+		}
+
+		long answer = (long) 1e12;
+		for (int j = 0; j <= N; j++) {
+			for (int k = 0; k <= N; k++) {
+				answer = Math.min(answer, dp[N][j][k][M]);
+			}
+		}
+		System.out.println(answer);
+	}
+}
+
+/*
 import java.util.Scanner;
 
 public class Main {
@@ -78,3 +147,4 @@ public class Main {
         System.out.println(ans);
     }
 }
+*/
