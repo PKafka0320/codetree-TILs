45818a50cfd0f5956cf4e4f18908a49d2c6377fe
PKafka <66552990+PKafka0320@users.noreply.github.com>
2024-12-16 12:25:00 +0900

    Title: bit 계산, Time: 343ms, Memory: 21MB, Status: Passed - CodeTree

--- 241216/bit 계산/bit-calculation.java
+++ 241216/bit 계산/bit-calculation.java
@@ -35,3 +35,60 @@ public class Main {
 		System.out.println(answer.toString());
 	}
 }
+
+/*
+import java.util.Scanner;
+
+public class Main {
+    // 변수 선언
+    public static int q;
+    
+    // 집합 S의 현재 상황을 하나의 정수 값으로 나타내어 관리합니다.
+    // S에 존재하는 수가 x1, x2, ..., xk라 한다면
+    // snum = 2^x1 + 2^x2 + .. 2^xk가 되도록 합니다.
+    public static int snum;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // 입력:
+        q = sc.nextInt();
+        while(q-- > 0) {
+            String command = sc.next();
+
+            if(command.equals("add")) {
+                int x = sc.nextInt();
+                
+                // 집합 S에 x를 추가합니다.
+                // 이미 x가 집합 S에 들어가 있지 않은 경우에만 넣어줍니다.
+                if(((snum >> x) & 1) == 0)
+                    snum ^= (1 << x);
+            }
+            else if(command.equals("delete")) {
+                int x = sc.nextInt();
+
+                // 집합 S에 x를 제거힙니다.
+                // 이미 x가 집합 S에 들어가 있는 경우에만 제거합니다.
+                if(((snum >> x) & 1) == 1)
+                    snum ^= (1 << x);
+            }
+            else if(command.equals("print")) {
+                int x = sc.nextInt();
+
+                // 집합 S에 x가 있는지 확인합니다.
+                System.out.println(((snum >> x) & 1));
+            }
+            else if(command.equals("toggle")) {
+                int x = sc.nextInt();
+
+                // 집합 S에 x를 toggle해줍니다.
+                snum ^= (1 << x);
+            }
+            else {
+                // 집합 S를 공집합으로 만들어줍니다.
+                snum = 0;
+            }
+        }
+    }
+}
+*/
