f2985f192eb5c3e0334502ac857e3d152f6082fc
PKafka <66552990+PKafka0320@users.noreply.github.com>
2024-12-19 13:23:57 +0900

    Title: n개의 합 최대화하기, Time: 884ms, Memory: 81MB, Status: Passed - CodeTree

--- 241219/n개의 합 최대화하기/maximize-n-sums.java
+++ 241219/n개의 합 최대화하기/maximize-n-sums.java
@@ -4,7 +4,6 @@ import java.util.*;
 public class Main {
 
 	public static void main(String[] args) throws Exception {
-		System.setIn(new FileInputStream("res/input.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		StringTokenizer st = new StringTokenizer(br.readLine());
