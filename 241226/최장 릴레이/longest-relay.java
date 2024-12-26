41faf943f3e4ba1275813cb37d4482d989e33577
PKafka <66552990+PKafka0320@users.noreply.github.com>
2024-12-26 13:21:30 +0900

    Title: 최장 릴레이, Time: 219ms, Memory: 13MB, Status: Passed - CodeTree

--- 241226/최장 릴레이/longest-relay.java
+++ 241226/최장 릴레이/longest-relay.java
@@ -24,19 +24,21 @@ public class Main {
 		}
 		dp[0][1 << 0] = 0;
 
-		for (int from = 0; from < N; from++) {
-			for (int mask = 0; mask < (1 << N); mask++) {
-				if ((mask & (1 << from)) == 0 || dp[from][mask] == -1)
-					continue;
-
-				for (int to = 0; to < N; to++) {
-					if ((mask | (1 << to)) == mask || dp[from][mask] >= grid[from][to])
+		for (int i = 0; i < N; i++) {
+			for (int from = 0; from < N; from++) {
+				for (int mask = 0; mask < (1 << N); mask++) {
+					if ((mask & (1 << from)) == 0 || dp[from][mask] == -1)
 						continue;
 
-					if (dp[to][mask | (1 << to)] == -1) {
-						dp[to][mask | (1 << to)] = grid[from][to];
-					} else {
-						dp[to][mask | (1 << to)] = Math.min(dp[to][mask | (1 << to)], grid[from][to]);
+					for (int to = 0; to < N; to++) {
+						if ((mask | (1 << to)) == mask || dp[from][mask] >= grid[from][to])
+							continue;
+
+						if (dp[to][mask | (1 << to)] == -1) {
+							dp[to][mask | (1 << to)] = grid[from][to];
+						} else {
+							dp[to][mask | (1 << to)] = Math.min(dp[to][mask | (1 << to)], grid[from][to]);
+						}
 					}
 				}
 			}
@@ -45,23 +47,22 @@ public class Main {
 		int answer = 0;
 		for (int end = 0; end < N; end++) {
 			for (int mask = 0; mask < (1 << N); mask++) {
-				if (dp[end][mask] == -1)
-					continue;
-
+				if (dp[end][mask] == -1) continue;
+				
 				answer = Math.max(answer, countPeople(mask));
 			}
 		}
 		System.out.println(answer);
 	}
-
+	
 	public static int countPeople(int mask) {
 		int result = 0;
-
+		
 		while (mask > 0) {
 			result += (mask & 1) == 1 ? 1 : 0;
 			mask >>= 1;
 		}
-
+		
 		return result;
 	}
 }
